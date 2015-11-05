fc8c98da36d86b805d088cb41d40e990ef5d6a76
Razvan Panea <razvan.panea@duke.edu>
2015-11-05 14:39:17 -0500

    Implemented validation function for SamtoolsBAMMerge

--- GAP_modules/SamtoolsBAMMerge.py
+++ GAP_modules/SamtoolsBAMMerge.py
@@ -9,10 +9,13 @@ class SamtoolsBAMMerge(Main):
         self.nr_splits    = 0
 
     def merge(self):
-        
         bam_splits = ["split_%d.bam" % i for i in range(nr_splits)]
         
         if self.sorted_input:
             return "samtools merge out.bam %s" % " ".join(bam_splits)
         else:
             return "samtools cat -o out.bam %s" % " ".join(bam_splits)
+
+    def validate(self):
+        if self.nr_splits == 0:
+            self.error("Number of splits was not set before merging!")
