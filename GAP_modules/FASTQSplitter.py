89701cef1bd8dbe64526fff67ac69c715fafcfa6
Razvan Panea <razvan.panea@duke.edu>
2015-11-05 12:46:20 -0500

    Fix #16. Modified FASTQSplitter for pair-end.

--- GAP_modules/FASTQSplitter.py
+++ GAP_modules/FASTQSplitter.py
@@ -2,10 +2,11 @@ from GAP_interfaces import Main
 
 class FASTQSplitter(Main):
 
-    def __init__(self, config, file_path):
+    def __init__(self, config, file_path, type):
         Main.__init__(self, config)
 
         self.file_path  = file_path
+        self.type       = type
 
     def byNrReads(self, nr_reads):
         
@@ -21,11 +22,21 @@ class FASTQSplitter(Main):
           
             while not done:
     
-                # Creating a new split file
+                # Creating a new split file, considering the original fastq type
                 split_count    += 1
-                split_filename  = "split_%d.fastq" % split_count
-                split_filepath  = "%s/%s" % (self.general.temp_dir, split_filename)
                 
+                if self.type == "PE_R1":
+                    split_filename  = "split_R1_%d.fastq" % split_count
+                elif self.type == "PE_R2":
+                    split_filename  = "split_R2_%d.fastq" % split_count
+                elif self.type == "SE":
+                    split_filename  = "split_%d.fastq" % split_count
+                else:
+                    self.warning("Unrecognized FASTQ file type '%s' in the pipeline. Default: Single-End.") 
+                    split_filename  = "split_%d.fastq" % split_count
+
+                split_filepath  = "%s/%s" % (self.general.temp_dir, split_filename)
+
                 self.message("Writing to split file %s." % split_filename)
 
                 # Writing to the new split file
