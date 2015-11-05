8a2f39c40b00b245821139b700b53bf39bc726a9
Razvan Panea <razvan.panea@duke.edu>
2015-11-05 12:24:41 -0500

    Fix #12. Implemented Samtools BAM Merger module.

---
+++ GAP_modules/SamtoolsBAMMerge.py
@@ -0,0 +1,18 @@
+from GAP_interfaces import Main
+
+class SamtoolsBAMMerge(Main):
+
+    def __init__(self, config):
+        Main.__init__(self, config)
+
+        self.sorted_input = False
+        self.nr_splits    = 0
+
+    def merge(self):
+        
+        bam_splits = ["split_%d.bam" % i for i in range(nr_splits)]
+        
+        if self.sorted_input:
+            return "samtools merge out.bam %s" % " ".join(bam_splits)
+        else:
+            return "samtools cat -o out.bam %s" % " ".join(bam_splits)
--- GAP_modules/__init__.py
+++ GAP_modules/__init__.py
@@ -1,3 +1,4 @@
 from SLURM import SLURM
 from BwaAligner import BwaAligner
 from FASTQSplitter import FASTQSplitter
+from SamtoolsBAMMerge import SamtoolsBAMMerge
