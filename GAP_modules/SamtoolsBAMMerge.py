5fdc13818ff5f9adcd0d96181eb352522119c456
Razvan Panea <razvan.panea@duke.edu>
2015-11-12 15:57:59 -0500

    Added and implemented Samtools path and temporary directory in SamtoolsBAMMerge.

--- GAP_modules/SamtoolsBAMMerge.py
+++ GAP_modules/SamtoolsBAMMerge.py
@@ -5,16 +5,19 @@ class SamtoolsBAMMerge(Main):
     def __init__(self, config):
         Main.__init__(self, config)
 
+        self.samtools_path= config.paths.samtools
+        self.temp_dir     = config.general.temp_dir
+
         self.sorted_input = False
         self.nr_splits    = 0
 
-    def merge(self):
-        bam_splits = ["split_%d.bam" % (i+1) for i in range(nr_splits)]
+    def getCommand(self):
+        bam_splits = ["%s/split_%d.bam" % (self.temp_dir, i+1) for i in range(self.nr_splits)]
         
         if self.sorted_input:
-            return "samtools merge out.bam %s" % " ".join(bam_splits)
+            return "%s merge %s/out.bam %s" % (self.samtools_path, self.temp_dir, " ".join(bam_splits))
         else:
-            return "samtools cat -o out.bam %s" % " ".join(bam_splits)
+            return "%s cat -o %s/out.bam %s" % (self.samtools_path, self.temp_dir, " ".join(bam_splits))
 
     def validate(self):
         if self.nr_splits == 0:
