e01bb90c3c80fad96ed4aa51853ba67a5e9cf6fb
Razvan Panea <razvan.panea@duke.edu>
2015-12-01 13:37:54 -0500

    Fixes module SamtoolsBAMMerge.

--- GAP_modules/SamtoolsBAMMerge.py
+++ GAP_modules/SamtoolsBAMMerge.py
@@ -10,13 +10,18 @@ class SamtoolsBAMMerge(Main):
 
         self.sorted_input = False
         self.nr_splits    = 0
+        self.threads      = -1
 
     def getCommand(self):
-        bam_splits = ["%s/bam_%04d" % (self.temp_dir, i) for i in range(self.nr_splits)]
-        
+
+        if self.threads == -1:
+            self.error("In merger implementation, the number of threads is not specified!")
+
         if self.sorted_input:
-            return "%s merge %s/out.bam %s" % (self.samtools_path, self.temp_dir, " ".join(bam_splits))
+            bam_splits = ["%s/bam_%04d.bam" % (self.temp_dir, i) for i in range(self.nr_splits)]
+            return "%s merge -@%d %s/out.bam %s" % (self.samtools_path, self.threads, self.temp_dir, " ".join(bam_splits))
         else:
+            bam_splits = ["%s/bam_%04d" % (self.temp_dir, i) for i in range(self.nr_splits)]
             return "%s cat -o %s/out.bam %s" % (self.samtools_path, self.temp_dir, " ".join(bam_splits))
 
     def validate(self):
