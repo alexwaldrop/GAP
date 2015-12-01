90648b709094db5ea7e0a8d47851ef5277337505
Razvan Panea <razvan.panea@duke.edu>
2015-12-01 13:38:22 -0500

    Fixes module SamtoolsBAMSorter.

--- GAP_modules/SamtoolsBAMSorter.py
+++ GAP_modules/SamtoolsBAMSorter.py
@@ -3,9 +3,9 @@ from GAP_interfaces import Sorter
 class SamtoolsBAMSorter(Sorter):
     
     def __init__(self, config):
-        Converter.__init__(self, config)
+        Sorter.__init__(self, config)
 
-        self.samtools_path  = config.path.samtools
+        self.samtools_path  = config.paths.samtools
         
         self.input_type     = "bam"
         self.output_type    = "bam"
@@ -14,10 +14,14 @@ class SamtoolsBAMSorter(Sorter):
         self.to_stdout      = False
 
         self.threads        = -1
-        self.out_prefix     = "split_sorted_"
+        self.temp_dir       = config.general.temp_dir
+        self.prefix         = ""
 
     def getCommand(self):
         if self.threads  == -1:
             self.error("In sorter implementation, number of threads not specified!")
 
-        return "%s sort -@ %d - %s" % (self.samtools_path, self.threads, self.out_prefix) 
+        if self.prefix == "":
+            self.error("In sorter implementation, the prefix is not specified!")
+
+        return "%s sort -@ %d - %s/%s" % (self.samtools_path, self.threads, self.temp_dir, self.prefix) 
