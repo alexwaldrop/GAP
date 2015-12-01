3eee3d734945f885c7e4e1008e5d7daa90a1993c
Razvan Panea <razvan.panea@duke.edu>
2015-12-01 13:39:12 -0500

    Integrates module SamtoolsBAMSorter in the GAP_modules package.

--- GAP_modules/__init__.py
+++ GAP_modules/__init__.py
@@ -3,3 +3,4 @@ from BwaAligner import BwaAligner
 from FASTQSplitter import FASTQSplitter
 from SamtoolsBAMMerge import SamtoolsBAMMerge
 from SamtoolsSamToBam import SamtoolsSamToBam
+from SamtoolsBAMSorter import SamtoolsBAMSorter
