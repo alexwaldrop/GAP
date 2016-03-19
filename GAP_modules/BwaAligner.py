b907af7417df2e5eeb421eae053b8f871f11cbaa
Razvan Panea <razvan.panea@duke.edu>
2016-03-19 17:29:11 -0400

    Fixes #32. Implements getters for required resources.

--- GAP_modules/BwaAligner.py
+++ GAP_modules/BwaAligner.py
@@ -16,12 +16,19 @@ class BwaAligner(Aligner):
 
         self.R1             = None
         self.R2             = None
+        
         self.threads        = -1
+        self.mem            = 20
+
+    def getCPURequirement(self):
+        return self.threads
+
+    def getMemRequirement(self):
+        return self.mem
 
     def getCommand(self):
 
         if self.threads == -1:
             self.error("In aligner implementation, number of threads not specified")
 
-        return "%s mem -M -t %d %s %s %s" % (self.config.paths.bwa, self.threads, self.config.aligner.ref, self.R1, self.R2) 
-        
+        return "%s mem -M -t %d %s %s %s" % (self.config.paths.bwa, self.threads, self.config.aligner.ref, self.R1, self.R2)
