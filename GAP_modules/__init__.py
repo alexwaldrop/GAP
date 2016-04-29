d9d09cdc0333b2e297ae8a984fe70ee5a9a7c5f4
Razvan Panea <razvan.panea@duke.edu>
2016-04-29 17:40:41 -0400

    Modifies the order of modules importing in the GAP_modules package.

--- GAP_modules/__init__.py
+++ GAP_modules/__init__.py
@@ -1,8 +1,8 @@
 from SLURM import SLURM
 from GoogleCompute import GoogleCompute
 
-from BwaAligner import BwaAligner
 from FASTQSplitter import FASTQSplitter
 from SamtoolsBAMMerge import SamtoolsBAMMerge
 from SamtoolsSamToBam import SamtoolsSamToBam
 from SamtoolsBAMSorter import SamtoolsBAMSorter
+from BwaAligner import BwaAligner
