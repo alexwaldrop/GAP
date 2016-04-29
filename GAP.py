0f45bc08abe654ffdfba1e5f1197c1b37aea8c4d
Razvan Panea <razvan.panea@duke.edu>
2016-04-29 17:41:47 -0400

    Fixes #40. Cleans the main script and improves the BWA module.

--- GAP.py
+++ GAP.py
@@ -1,111 +1,32 @@
 #!/usr/bin/env python2.7
 
-from GAP_system import Config, TaskManager, Task
+import time
+from GAP_system import Config
+from GAP_modules import BwaAligner as Aligner
+from GAP_modules import GoogleCompute as Platform
 
 # Generating the config object
-config = Config("GAP.config")
+config = Config("GAP.config", silent = True)
 
-from GAP_modules import FASTQSplitter as Splitter
-from GAP_modules import SamtoolsSamToBam as ConverterSamToBam
-from GAP_modules import SamtoolsBAMSorter as BAMSorter
-from GAP_modules import SamtoolsBAMMerge as BAMMerger
+# Setting up a fake profile
+s = { "R1_path":"gs://davelab_temp/R1_40M_TEST.fastq",
+      "R2_path":"gs://davelab_temp/R2_40M_TEST.fastq"}
 
-if config.cluster.ID == 0:
-    from GAP_modules import SLURM as Cluster
+# Setting up the platform
+plat = Platform(config)
+plat.prepareData(s, nr_local_ssd=2)
 
-if config.aligner.ID == 0:
-    from GAP_modules import BwaAligner as Aligner
+# Setting up the aligner
+align = Aligner(config)
+align.R1 = s["R1_new_path"]
+align.R2 = s["R2_new_path"]
+align.threads = 32
 
-splitter    = Splitter(config)
-converter   = ConverterSamToBam(config)
-sorter      = BAMSorter(config)
-merger      = BAMMerger(config)
-cluster     = Cluster(config)
-aligner     = Aligner(config)
-task_manager = TaskManager(config, cluster)
+print (align.getCommand())
 
-splitted = config.cluster.nodes > 1
+# Running the alignment command
+plat.runCommand("align", align.getCommand(), on_instance = plat.main_server).wait()
 
-# Splitting the FASTQ file(s)
-if splitted:
-    task = Task("split", config, cluster)
-
-    task.type       = "split"
-    task.nodes      = 1
-    task.mincpus    = 1
-    task.command    = splitter.byNrReads(int(1e7), config.paths.R1, config.paths.R2)
-
-    task_manager.addTask(task)
-
-    # Getting the number of splits
-    split_count = splitter.split_count
-
-if config.general.goal == "align":
-    if splitted:
-        for split_id in range(split_count):
-            task = Task("align_%d" % split_id, config, cluster)
-            task.type       ="align"
-
-            task.nodes      = 1
-            task.mincpus    = config.cluster.mincpus
-            task.requires   = ["split"]
-
-            aligner.R1      = "%s/fastq_R1_%04d" % (config.general.temp_dir, split_id)
-            aligner.R2      = "%s/fastq_R2_%04d" % (config.general.temp_dir, split_id)
-            aligner.threads = task.mincpus
-
-            converter.threads = task.mincpus
-
-            sorter.threads  = task.mincpus
-            sorter.prefix   = "bam_%04d" % split_id
-            if aligner.to_stdout:
-                if aligner.output_type == "sam":
-                    task.command = "%s | %s | %s" % (aligner.getCommand(), converter.getCommand(), sorter.getCommand())
-                else:
-                    task.command = "%s | %s" % (aligner.getCommand(), sorter.getCommand())
-            else:
-                task.command = aligner.getCommand()
-
-            task_manager.addTask(task)
-    else:
-        task = Task("align", config, cluster)
-        task.type     = "align"
-        
-        task.nodes    = 1
-        task.mincpus  = config.cluster.mincpus
-        
-        aligner.R1      = config.paths.R1
-        aligner.R2      = config.paths.R2
-        aligner.threads = task.mincpus
-
-        converter.threads = task.mincpus
-
-        sorter.threads  = task.micpus
-        sorter.prefix   = "out"
-        if aligner.to_stdout:
-            if aligner.output_type == "sam":
-                task.command = "%s | %s | %s" % (aligner.getCommand(), converter.getCommand(),sorter.getCommand())
-            else:
-                task.command = "%s | %s" % (aligner.getCommand(), sorter.getCommand())
-        else:
-            task.command = aligner.getCommand()
-
-        task_manager.addTask(task)
-
-
-if splitted:
-    task = Task("merge", config, cluster)
-    task.type       = "merge"
-
-    task.nodes      = 1
-    task.mincpus    = config.cluster.mincpus
-    task.requires   = ["align_%d" % split_id for split_id in range(split_count)]
-
-    merger.threads  = task.mincpus
-    merger.sorted_input = True
-    merger.nr_splits= split_count
-    task.command    = merger.getCommand()
-
-    task_manager.addTask(task)
-
-task_manager.run()
+# Aligning done
+print ("DONE!")
+time.sleep(300)
--- GAP_modules/BwaAligner.py
+++ GAP_modules/BwaAligner.py
@@ -1,5 +1,8 @@
 from GAP_interfaces import Aligner
 
+from GAP_modules import SamtoolsSamToBam as ConverterSamToBam
+from GAP_modules import SamtoolsBAMSorter as BAMSorter
+
 class BwaAligner(Aligner):
     
     def __init__(self, config):
@@ -17,8 +20,11 @@ class BwaAligner(Aligner):
         self.R1             = None
         self.R2             = None
         
-        self.threads        = -1
+        self.threads        = 1
         self.mem            = 20
+
+        self.sam_to_bam     = ConverterSamToBam(config)
+        self.bam_sort       = BAMSorter(config)
 
     def getCPURequirement(self):
         return self.threads
@@ -31,4 +37,13 @@ class BwaAligner(Aligner):
         if self.threads == -1:
             self.error("In aligner implementation, number of threads not specified")
 
-        return "%s mem -M -t %d %s %s %s" % (self.config.paths.bwa, self.threads, self.config.aligner.ref, self.R1, self.R2)
+        aligner_cmd     = "%s mem -M -t %d %s %s %s" % (self.config.paths.bwa, self.threads, self.config.aligner.ref, self.R1, self.R2)
+
+        self.sam_to_bam.threads = self.threads
+        sam_to_bam_cmd  = self.sam_to_bam.getCommand()
+
+        self.bam_sort.threads   = self.threads
+        self.bam_sort.prefix    = "out"
+        bam_sort_cmd    = self.bam_sort.getCommand()
+
+        return "%s | %s | %s" % (aligner_cmd, sam_to_bam_cmd, bam_sort_cmd)
