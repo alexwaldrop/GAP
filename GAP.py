f990f831f2191b40d2c586fe92d81471ad3b21fc
Razvan Panea <razvan.panea@duke.edu>
2017-05-12 15:01:33 -0400

    Adds colored logging.

--- GAP.py
+++ GAP.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2.7
 
 import logging
+import sys
 
 from GAP_config import Config
 from GAP_system import NodeManager
@@ -11,28 +12,47 @@ from GAP_modules.Google import GoogleException
 config = None
 plat = None
 
+def configure_logging(config):
+
+    # Setting the format of the logs
+    FORMAT = "[%(asctime)s] %(levelname)s: %(message)s"
+
+    # Configuring the logging system to the lowest level
+    logging.basicConfig(level=logging.DEBUG, format=FORMAT, stream=sys.stderr)
+
+    # Defining the ANSI Escape characters
+    BOLD = '\033[1m'
+    DEBUG = '\033[92m'
+    INFO = '\033[94m'
+    WARNING = '\033[93m'
+    ERROR = '\033[91m'
+    END = '\033[0m'
+
+    # Coloring the log levels
+    if sys.stderr.isatty():
+        logging.addLevelName(logging.ERROR,     "%s%s%s%s%s" % (BOLD, ERROR,    "GAP_ERROR",    END, END))
+        logging.addLevelName(logging.WARNING,   "%s%s%s%s%s" % (BOLD, WARNING,  "GAP_WARNING",  END, END))
+        logging.addLevelName(logging.INFO,      "%s%s%s%s%s" % (BOLD, INFO,     "GAP_INFO",     END, END))
+        logging.addLevelName(logging.DEBUG,     "%s%s%s%s%s" % (BOLD, DEBUG,    "GAP_DEBUG",    END, END))
+    else:
+        logging.addLevelName(logging.ERROR,     "GAP_ERROR")
+        logging.addLevelName(logging.WARNING,   "GAP_WARNING")
+        logging.addLevelName(logging.INFO,      "GAP_INFO")
+        logging.addLevelName(logging.DEBUG,     "GAP_DEBUG")
+
+    # Setting the level of the logs
+    level = [logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG][ config["general"]["verbosity"] ]
+    logging.getLogger().setLevel(level)
+
 def main():
     global plat
     global config
 
-    # Setting the format of the logs
-    FORMAT = "[%(asctime)s] GAP_%(levelname)s: %(message)s"
-
-    # Configuring the logging system to the lowest level
-    logging.basicConfig(level=logging.DEBUG, format=FORMAT)
-
-    # Creating logging
+    # Obtain the config object
     config = Config("GAP_config/GAP.config").config
 
-    # Setting the level of the logs
-    if config["general"]["verbosity"] == 0:
-        logging.getLogger().setLevel(logging.ERROR)
-    elif config["general"]["verbosity"] == 1:
-        logging.getLogger().setLevel(logging.WARNING)
-    elif config["general"]["verbosity"] == 2:
-        logging.getLogger().setLevel(logging.INFO)
-    else:
-        logging.getLogger().setLevel(logging.DEBUG)
+    # Configure the logging system
+    configure_logging(config)
 
     # Create platform
     plat = Platform(config)
