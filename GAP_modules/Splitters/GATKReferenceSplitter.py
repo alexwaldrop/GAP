daee5e265ee8aef31b8e9d63faed1d87cde5224a
Alex Waldrop <amw117@duke.edu>
2017-06-06 17:14:25 -0400

    Fixes #104. Chromosome splits determined on the fly.
    Splitter modules which split bam files by chromosome no longer rely on information in the config to split.
    Chromosomes to split are determined at runtime from the bam index file.

--- GAP_modules/Splitters/GATKReferenceSplitter.py
+++ GAP_modules/Splitters/GATKReferenceSplitter.py
@@ -1,4 +1,6 @@
 from GAP_interfaces import Splitter
+import logging
+import operator
 
 __main_class__ = "GATKReferenceSplitter"
 
@@ -7,11 +9,14 @@ class GATKReferenceSplitter(Splitter):
     def __init__(self, platform, tool_id, main_module_name=None):
         super(GATKReferenceSplitter, self).__init__(platform, tool_id, main_module_name)
 
-        self.input_keys     = ["bam"]
+        self.input_keys     = ["bam", "bam_idx"]
         self.output_keys    = ["bam", "BQSR_report", "location", "excluded_location"]
 
-        self.req_tools      = []
+        self.req_tools      = ["samtools"]
         self.req_resources  = []
+
+        # Number of splits BAM file will be divided among
+        self.nr_chrom_splits = self.config["general"]["nr_splits"]
 
     def init_split_info(self, **kwargs):
         # Obtain arguments
@@ -20,7 +25,8 @@ class GATKReferenceSplitter(Splitter):
 
         # Get information related to each split
         # Process each chromosome separately and process the rest in one single run
-        chrom_list = self.config["sample"]["chrom_list"]
+        chrom_list, remains = self.get_chrom_splits(bam)
+
         for chrom in chrom_list:
             self.output.append(
                 {
@@ -47,5 +53,48 @@ class GATKReferenceSplitter(Splitter):
         # No command needs to be run
         return None
 
+    def get_chrom_splits(self, bam):
+        # Returns two lists, one containing the names of chromosomes which will be considered separate splits
+        # And another containing the names of chromosomes that will be lumped together an considered one split
+        # Split chromosomes will be determined by the number of reads mapped to each chromosome
 
+        # Obtaining the chromosome alignment information
+        # Output sorted in descending order by the number of reads mapping to each chromosome
+        main_instance = self.platform.get_main_instance()
+        cmd = "%s idxstats %s | sort -nrk 3" % (self.tools["samtools"], bam)
+        main_instance.run_command("gatk_bam_splitter_idxstats", cmd, log=False)
+        out, err = main_instance.get_proc_output("gatk_bam_splitter_idxstats")
 
+        if err != "":
+            err_msg = "Could not obtain information for %s. " % self.main_module_name
+            err_msg += "The following command was run: \n  %s. " % cmd
+            err_msg += "The following error appeared: \n  %s." % err
+            logging.error(err_msg)
+            exit(1)
+
+        # Analysing the output of idxstats to identify the number of reads mapping to each chromosome
+        sorted_chrom_names = list()
+        for line in out.split("\n"):
+            # Skip empty lines
+            if len(line) == 0:
+                continue
+
+            data = line.split()
+
+            # Skip unmapped reads
+            if data[0] == "*":
+                continue
+
+            # Add name of next chromosome
+            sorted_chrom_names.append(data[0])
+
+        # Create list of split and lumped chromosome by adding chromosomes in order of size until number of splits is reached
+        chroms = list()
+        i = 0
+        while (i < self.nr_chrom_splits-1) and (len(sorted_chrom_names) > 0):
+            # Create split for next largest chromosome if any more chromosomes are left
+            chroms.append(sorted_chrom_names.pop(0))
+            i += 1
+
+        remains = sorted_chrom_names
+        return chroms, remains
