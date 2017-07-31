aa6aa1ba989e21da161cde08c03d0c3d1920c9b8
Alex Waldrop <amw117@duke.edu>
2017-07-31 16:19:49 -0400

    Fixes #133. Implements JsonParser class from new schema.

---
+++ new_schema/JsonParser.py
@@ -0,0 +1,46 @@
+import json
+import logging
+
+from jsonschema import Draft4Validator
+from BaseParser import BaseParser
+
+class JsonParser(BaseParser):
+    # Class for parsing information from a JSON config file
+    def __init__(self, config_file, config_spec_file):
+        super(JsonParser, self).__init__(config_file, config_spec_file)
+
+    def __read_config(self):
+        # Parse config data from JSON file and return JSON
+        return self.parse_json(self.config_file)
+
+    def __validate_config(self):
+        # Parse config spec file
+        spec = self.parse_json(self.config_spec_file)
+
+        # create validator object
+        validator = Draft4Validator(spec)
+
+        # validate config against schema and throw errors if invalid
+        try:
+            validator.validate(self.config)
+        except:
+            # raise invalid schema error and return messages
+            logging.error("Invalid config: %s" % self.config_file)
+            errors = validator.iter_errors(self.config)
+            for error in sorted(errors):
+                logging.error(error.message)
+            raise
+
+    @staticmethod
+    def parse_json(json_file):
+        # read JSON formatted instance template config
+        with open(json_file, "r") as fh:
+            json_data = fh.read()
+
+        # try to load json from file, return exception otherwise
+        try:
+            return json.loads(json_data)
+
+        except:
+            logging.error("Config parsing error! Input File is not valid JSON: %s" % json_file)
+            raise
