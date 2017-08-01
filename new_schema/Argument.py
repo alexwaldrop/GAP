9bfbdf67c5d92d80c51020a5de29f8d694744c13
Razvan Panea <razvan.panea@duke.edu>
2017-08-01 14:19:44 -0400

    Fixes #137. Implements class "Argument".

---
+++ new_schema/Argument.py
@@ -0,0 +1,31 @@
+
+class Argument(object):
+
+    def __init__(self, name, is_required=False, is_resource=False, default_value=None):
+
+        self.name = name
+
+        self.__is_required = is_required
+        self.__is_resource = is_resource
+
+        self.__default_value = default_value
+
+        self.__value = None
+
+    def set(self, value):
+        self.value = value
+
+    def get_default_value(self):
+        return self.__default_value
+
+    def get_value(self):
+        return self.value
+
+    def is_set(self):
+        return self.value is not None
+
+    def is_mandatory(self):
+        return self.__is_required
+
+    def is_resource(self):
+        return self.__is_resource
