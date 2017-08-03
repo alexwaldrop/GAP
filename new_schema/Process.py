35d3457396cf8d0af10b39d335c019d7e91a6abc
Alex Waldrop <amw117@duke.edu>
2017-08-03 11:37:07 -0400

    Fixed #157. Implemented 'Process' class from new schema.

---
+++ new_schema/Process.py
@@ -0,0 +1,17 @@
+import subprocess as sp
+
+class Process(sp.Popen):
+
+    def __init__(self, args, **kwargs):
+        self.command    = kwargs.pop("cmd",     True)
+        super(Process, self).__init__(args,     **kwargs)
+
+    def is_complete(self):
+        return self.poll() is not None
+
+    def has_failed(self):
+        ret_code = self.poll()
+        return ret_code is not None and ret_code != 0
+
+    def get_command(self):
+        return self.command
