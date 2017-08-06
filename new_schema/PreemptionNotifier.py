2f3b5060b04bf70adfbb6e9656a8160ceb9042cc
Alex Waldrop <amw117@duke.edu>
2017-08-06 14:28:15 -0400

    Fixes #151. Implements PreemptionNotifier from new schema.

---
+++ new_schema/PreemptionNotifier.py
@@ -0,0 +1,129 @@
+import logging
+import json
+from LogSink import LogSink
+from PubSub import PubSub
+from GooglePreemptibleProcessor import GooglePreemptibleProcessor
+import threading
+import time
+
+class PreemptionNotifier(threading.Thread):
+
+    def __init__(self, name, processors, key_file):
+        super(PreemptionNotifier, self).__init__()
+
+        self.name       = name
+
+        # Dictionary of preemptible processors being watched by notifier
+        self.processors = processors
+
+        # Google cloud key file containing account metadata
+        self.key_file   = key_file
+
+        # Boolean flag indicating whether notifier is currently running
+        self.running    = False
+
+        # Boolean flag indicating that notifier is a daemon running in the background
+        self.daemon     = True
+
+        # Initialize PubSub topic/subscription for capturing/accesing logging info
+        self.preempt_topic  = "preempted_topic_%s" % self.name
+        self.preempt_sub    = "preempted_sub_%s" % self.name
+        self.pub_sub        = self.__init_pubsub()
+
+        # Initialize log sink to funnel messages from Google Stackdriver logging to PubSub topic
+        self.google_project     = self.__get_key_field("project_id")
+        self.log_sink_name      = "preempted_sink_%s" % self.name
+        self.log_sink_dest      = "pubsub.googleapis.com/projects/%s/topics/%s" % (self.google_project, self.preempt_topic)
+        self.log_sink_filter    = "jsonPayload.event_subtype:compute.instances.preempted"
+        self.log_sink           = self.__init_log_sink()
+
+        # Start the preemption notifier
+        self.start()
+
+    def __init_pubsub(self):
+        # Create PubSub topics and subscriptions for storing/accessing information related to instance preemption
+        logging.debug("Configuring Google Pub/Sub for preemption notifier...")
+        pubsub = PubSub()
+
+        # Create topic and subscription to that topic
+        logging.debug("Creating PubSub topic '%s'..." % self.preempt_topic)
+        pubsub.create_topic(self.preempt_topic)
+        logging.debug("Creating subscription '%s' to topic '%s'..." % (self.preempt_sub, self.preempt_topic))
+        pubsub.create_subscription(self.preempt_sub, self.preempt_topic)
+
+        logging.info("Google Pub/Sub configured for preemption notifier.")
+        return pubsub
+
+    def __init_log_sink(self):
+        # Create log sink to send preemption events to PubSub preempt topic
+        logging.debug("Creating Google Logging sink to send preemption events to Google Pub/Sub.")
+        log_sink = LogSink(self.log_sink_name, self.log_sink_dest, log_filter=self.log_sink_filter)
+        logging.info("Preemption logging sink '%s' successfully created!" % self.log_sink_name)
+
+        # Grant write permission to allow the preempted log sink to write to the preempted PubSub topic
+        self.pub_sub.grant_write_permission(topic=self.preempt_topic,
+                                            client_json_keyfile=self.key_file,
+                                            serv_acct=self.log_sink.get_serv_acct())
+        return log_sink
+
+    def run(self):
+        # Periodically pull next message from PubSub subscription and check to see if instances have been preempted
+        # If an instance has been preempted, set its instance status to DEAD
+        while self.running:
+
+            try:
+                msg, _ = PubSub.get_message(self.preempt_sub)
+
+                if msg is not None:
+                    self.process_message(msg)
+                else:
+                    time.sleep(2)
+
+            except BaseException, e:
+                if self.running:
+                    raise
+                else:
+                    if e.message != "":
+                        logging.debug(
+                            "Google Subscriber was forcefully stopped. The following exception message was received: %s."
+                            % e.message)
+                    else:
+                        logging.debug("Google Subscriber was forcefully stopped.")
+
+    def process_message(self, msg):
+        # Process a message pulled from the Preempted PubSub Subscription
+        # Message should be a Google log in JSON format
+        try:
+            log = json.loads(msg)
+            inst_name = log["jsonPayload"]["resource"]["name"]
+            if inst_name in self.processors:
+                # Set instance status to DEAD if preempted instance is mangaged by the notifier
+                self.processors[inst_name].set_status(GooglePreemptibleProcessor.DEAD)
+                logging.debug("(%s) Instance preempted!" % inst_name)
+        except ValueError:
+            logging.error("Preempted message should be a Google log in JSON format. The following message was received instead: %s." % msg)
+        except:
+            raise
+
+    def stop(self):
+        self.running = False
+
+    def clean_up(self):
+        # Remove PubSub topics and remove log sink
+        logging.info("Destroying preemption notifier...")
+        self.log_sink.destroy()
+        self.pub_sub.clean_up()
+
+    def __get_key_field(self, field_name):
+        # Parse JSON service account key file and return email address associated with account
+
+        # Parse json into dictionary
+        with open(self.key_file) as kf:
+            key_data = json.load(kf)
+
+        # Check to make sure correct key is present in dictionary
+        if field_name not in key_data:
+            logging.error("'%s' field missing from authentication key file: %s. Check to make sure key exists in file or that file is valid google key file!" %
+                          (field_name, self.key_file))
+            exit(1)
+        return key_data[field_name]
