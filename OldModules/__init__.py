c1768c8c0823d2dee6397aad5b9fe480ed04835d
Razvan Panea <razvan.panea@duke.edu>
2017-08-15 11:28:39 -0400

    Removes the unnecessary files.

--- OldModules/__init__.py
+++
@@ -1,6 +0,0 @@
-from ABCMetaEnhanced import ABCMetaEnhanced
-from Module import Module
-from Modules.Mergers.Merger import Merger
-from Modules.Tools.Tool import Tool
-
-__all__ = ["Splitter", "Tool", "Merger"]
