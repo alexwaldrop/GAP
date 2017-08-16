0e1315d4602a8e07d3ca98f4363ea16caeb5ed5a
Alex Waldrop <amw117@duke.edu>
2017-08-15 20:22:35 -0400

    Cleaned code for setting cpu/mem in ModuleWorker

--- System/Workers/ModuleWorker.py
+++ System/Workers/ModuleWorker.py
@@ -63,65 +63,52 @@ class ModuleWorker(Thread):
         # Get the arguments from the module
         arguments = self.module_obj.get_arguments()
 
-        # Set nr_cpus
-        nr_cpus_arg = arguments["nr_cpus"]
-        self.__set_argument("nr_cpus", nr_cpus_arg)
-        if not nr_cpus_arg.is_set():
-            # Set special case for maximum nr_cpus
-            if isinstance(nr_cpus_arg.get_default_value(), basestring):
-                if nr_cpus_arg.get_default_value().lower() == "max":
-                    nr_cpus_arg.set(self.platform.get_max_nr_cpus())
-            else:
-                nr_cpus_arg.set(nr_cpus_arg.get_default_value())
-
-        else:
-            # Make sure nr cpus is an integer
-            nr_cpus_arg.set(int(nr_cpus_arg.get_value()))
-
-        # Set mem
-        mem_arg = arguments["mem"]
-        self.__set_argument("mem", mem_arg)
-        if not mem_arg.is_set():
-            if isinstance(mem_arg.get_default_value(), basestring):
-                if mem_arg.get_default_value().lower() == "max":
-                    # Set special case for maximum mem
-                    mem_arg.set(self.platform.get_max_mem())
-                elif "nr_cpus" in mem_arg.get_default_value().lower():
-                    # Set special case if memory is scales with nr_cpus (e.g. 'nr_cpus * 1.5')
-                    nr_cpus     = nr_cpus_arg.get_value()
-                    mem_expr    = mem_arg.get_default_value().lower()
-                    mem         = int(eval(mem_expr.replace("nr_cpus", str(nr_cpus))))
-                    if mem < self.platform.get_max_mem():
-                        mem_arg.set(mem)
-                    else:
-                    # Set to max memory if amount of memory requested exceeds limit
-                        mem_arg.set(self.platform.get_max_mem())
-            # Set default value
-            else:
-                mem_arg.set(mem_arg.get_default_value())
-        else:
-            # Make sure mem is an integer
-            mem_arg.set(int(mem_arg.get_value()))
-
         # Set the rest of the args
         for arg_key, arg in arguments.iteritems():
 
-            if arg_key not in ["nr_cpus", "mem"]:
+            # Set the argument depending if it is a resource
+            if arg.is_resource():
+                self.__set_resource_argument(arg_key, arg)
+            else:
+                self.__set_argument(arg_key, arg)
 
-                # Set the argument depending if it is a resource
-                if arg.is_resource():
-                    self.__set_resource_argument(arg_key, arg)
-                else:
-                    self.__set_argument(arg_key, arg)
-
-                # If not set yet, set with the default variable
-                if not arg.is_set():
-                    arg.set(arg.get_default_value())
+            # If not set yet, set with the default variable
+            if not arg.is_set():
+                arg.set(arg.get_default_value())
 
             # If still not set yet, raise an exception if the argument is required
             if not arg.is_set() and arg.is_mandatory():
                 logging.error("In module %s, required input key \"%s\" could not be set." % (self.name, arg_key))
                 raise IOError("Input could not be provided to the module %s " % self.name)
+
+        # Make special changes to nr_cpus
+        nr_cpus = arguments["nr_cpus"]
+        if isinstance(nr_cpus.get_value(), basestring) and nr_cpus.get_value().lower() == "max":
+            # Set special case for maximum nr_cpus
+            nr_cpus.set(self.platform.get_max_nr_cpus)
+        else:
+            # Make sure nr cpus is an integer
+            nr_cpus.set(int(nr_cpus.get_value()))
+
+        # Make special changes to mem
+        mem = arguments["mem"]
+        if isinstance(mem.get_value(), basestring):
+            if mem.get_value().lower() == "max":
+                # Set special case where mem is max platform memory
+                mem.set(self.platform.get_max_mem())
+            elif "nr_cpus" in mem.get_value().lower():
+                # Set special case if memory is scales with nr_cpus (e.g. 'nr_cpus * 1.5')
+                nr_cpus     = nr_cpus.get_value()
+                mem_expr    = mem.get_default_value().lower()
+                mem_val     = int(eval(mem_expr.replace("nr_cpus", str(nr_cpus))))
+                if mem_val < self.platform.get_max_mem():
+                    mem.set(mem_val)
+                else:
+                    # Set to max memory if amount of memory requested exceeds limit
+                    mem.set(self.platform.get_max_mem())
+        else:
+            # Make sure mem is an integer
+            mem.set(int(mem.get_value()))
 
     def task(self):
 
