18c938c681432a57c022febfcad245802766d424
Alex Waldrop <amw117@duke.edu>
2017-08-23 12:42:35 -0400

    Fixes #163.
    Throws mroe informative error if graph is not correctly defined in config.

--- System/Graph/Graph.py
+++ System/Graph/Graph.py
@@ -1,3 +1,5 @@
+import logging
+
 from Config import ConfigParser
 from System.Graph import Node
 
@@ -12,6 +14,7 @@ class Graph(object):
 
         # Generate graph
         self.nodes, self.adj_list = self.__generate_graph()
+        self.__check_adjacency_list()
 
     def __generate_graph(self):
 
@@ -26,6 +29,17 @@ class Graph(object):
             nodes[node_id] = Node(node_id, **node_data)
 
         return nodes, adj_list
+
+    def __check_adjacency_list(self):
+        errors = False
+        for node, adj_nodes in self.adj_list.iteritems():
+            for adj_node in adj_nodes:
+                if adj_node not in self.nodes:
+                    logging.error("Incorrect pipeline graph! Node '%s' receives input from an undeclared node: '%s'. "
+                                  "Please correct in the pipeline graph config!" % (node, adj_node))
+                    errors = True
+        if errors:
+            raise IOError("Incorrect pipeline graph defined in graph config!")
 
     def get_adjacency_list(self):
         return self.adj_list
