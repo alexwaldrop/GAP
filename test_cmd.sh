4965484afe178b2bc66720ed656d69b11e145c08
Rachel Kositsky <rkositsk@andrew.cmu.edu>
2017-11-12 20:19:34 -0500

    Add Google Cloud account key
    - Added service account key so GAP will run on gcloud without hiccups
    - Edited output directory in GAP command to reference the gcloud bucket

--- test_cmd.sh
+++ test_cmd.sh
@@ -1,2 +1,2 @@
 # TODO: REMOVE THIS FILE BEFORE MERGING BRANCH!
-python Main.py --input Config/Templates/SCSample.json --name SC_test0 --pipeline_config Config/Templates/Single_Cell_Graph_10X.config --res_kit_config Config/Templates/ResourceKit.config --plat_config Config/Templates/Platform/GooglePlatform.config --plat_name Google -vvv -o sc_test0
+python Main.py --input Config/Templates/SCSample.json --name SC_test0 --pipeline_config Config/Templates/Single_Cell_Graph_10X.config --res_kit_config Config/Templates/ResourceKit.config --plat_config Config/Templates/Platform/GooglePlatform.config --plat_name Google -vvv -o gs://davelab_home/rk213/sc_test0
