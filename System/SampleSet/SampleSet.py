644d4615dbaba56259efdafd2e708ffab6f49b38
Razvan Panea <razvan.panea@duke.edu>
2017-11-20 16:10:56 -0500

    Adds minor changes.
    Makes distinction between directory path and file path.
    Works for directory paths that end in multiple "/".

--- System/SampleSet/SampleSet.py
+++ System/SampleSet/SampleSet.py
@@ -106,19 +106,15 @@ class SampleSet (object):
         # Searches in self.data for a path matching the src_path
         # Updates path to refelct transfer to dest_dir
 
-        # Remove last backslash if path is a directory
-        is_dir = src_path.endswith("/")
-        if is_dir:
-            src_path = src_path[:-1]
+        # Update source path path to the new path, depending if directory or not
+        if src_path.endswith("/"):
+            dir_name    = src_path.rstrip("/").split("/")[-1]
+            new_path    = os.path.join(dest_dir, dir_name)
+            new_path    += "/"
 
-        # Get name of file after transfer
-        file_name   = src_path.split("/")[-1]
-        new_path    = os.path.join(dest_dir, file_name)
-
-        # Re-append last backslash if directory
-        if is_dir:
-            src_path += "/"
-            new_path += "/"
+        else:
+            file_name   = src_path.split("/")[-1]
+            new_path    = os.path.join(dest_dir, file_name)
 
         # Search through paths to find the correct path to update
         path_types      = self.path_keys
