8d0b432bc8742a08b419cb5a92c3c5c0643d7a86
Alex Waldrop <amw117@duke.edu>
2017-12-11 13:27:12 -0500

    Minor fix of RecodeVCF module.
    Updated name of include-columns to 'info-columns' to be consistent with RecodeVCF.py script.

--- Modules/Tools/RecodeVCF.py
+++ Modules/Tools/RecodeVCF.py
@@ -13,7 +13,7 @@ class RecodeVCF(Module):
         self.add_argument("vcf",                is_required=True)                       # Input VCF file
         self.add_argument("recode_vcf",         is_required=True,   is_resource=True)   # Path to RecodeVCF.py executable
         self.add_argument("min-call-depth",     is_required=True,   default_value=10)   # Minimum reads supporting an allele to call a GT
-        self.add_argument("columns-to-include", is_required=False)                      # Optional list of INFO column names to include
+        self.add_argument("info-columns",       is_required=False)                      # Optional list of INFO column names to include in output
         self.add_argument("nr_cpus",            is_required=True,   default_value=1)
         self.add_argument("mem",                is_required=True,   default_value=2)
 
@@ -27,7 +27,7 @@ class RecodeVCF(Module):
         vcf_in              = self.get_arguments("vcf").get_value()
         recode_vcf_exec     = self.get_arguments("recode_vcf").get_value()
         min_call_depth      = self.get_arguments("min-call-depth").get_value()
-        columns_to_include  = self.get_arguments("columns-to-include").get_value()
+        info_columns        = self.get_arguments("info-columns").get_value()
 
         # Get final recoded VCF output file path
         recoded_vcf_out = self.get_output("recoded_vcf")
@@ -39,10 +39,10 @@ class RecodeVCF(Module):
         cmd = "python %s --vcf %s --output %s --min-call-depth %s -vvv" % (recode_vcf_exec, vcf_in, recoded_vcf_out, min_call_depth)
 
         # Optionally point to file specifying which vcf INFO fields to include in recoded output file
-        if isinstance(columns_to_include, list):
-            cmd += " --info-columns %s" % ",".join(columns_to_include)
-        elif isinstance(columns_to_include, basestring):
-            cmd += " --info-columns %s" % columns_to_include
+        if isinstance(info_columns, list):
+            cmd += " --info-columns %s" % ",".join(info_columns)
+        elif isinstance(info_columns, basestring):
+            cmd += " --info-columns %s" % info_columns
 
         # Capture stderr
         cmd += " !LOG3!"
