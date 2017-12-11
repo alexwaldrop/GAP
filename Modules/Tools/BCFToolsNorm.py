3118a4d9b886ac955ff7372fb9c3b168ea905997
Alex Waldrop <amw117@duke.edu>
2017-12-11 16:09:37 -0500

    Added BCFToolsNorm module.
    Module is used to normalize VCF files in the following ways:
    1) Split any multiallelic variants into new rows so 1 row = 1 variant.
    2) Left-normalize indels against the reference genome.

---
+++ Modules/Tools/BCFToolsNorm.py
@@ -0,0 +1,45 @@
+from Modules import Module
+
+class BCFToolsNorm(Module):
+
+    def __init__(self, module_id):
+        super(BCFToolsNorm, self).__init__(module_id)
+
+        self.input_keys     = ["bcftools", "vcf", "ref", "nr_cpus", "mem"]
+        self.output_keys    = ["vcf"]
+        self.quick_command  = True
+
+    def define_input(self):
+        self.add_argument("vcf",                is_required=True)                       # Input VCF file
+        self.add_argument("bcftools",           is_required=True,   is_resource=True)
+        self.add_argument("ref",                is_required=True,   is_resource=True)   # Path to Fasta formatted genome reference
+        self.add_argument("split_multiallelic", is_required=True,   default_value=True) # Whether to split multiallelic sites into mutliple sites
+        self.add_argument("nr_cpus",            is_required=True,   default_value=4)
+        self.add_argument("mem",                is_required=True,   default_value="nr_cpus*2")
+
+    def define_output(self, platform, split_name=None):
+        # Declare recoded VCF output filename
+        normalized_vcf = self.generate_unique_file_name(split_name=split_name, extension=".normalized.vcf")
+        self.add_output(platform, "vcf", normalized_vcf)
+
+    def define_command(self, platform):
+        # Get input arguments
+        vcf_in              = self.get_arguments("vcf").get_value()
+        bcftools            = self.get_arguments("bcftools").get_value()
+        ref                 = self.get_arguments("ref").get_value()
+        split_multiallelic  = self.get_arguments("split_multiallelic").get_value()
+
+        # Get final normalized VCF output file path
+        vcf_out = self.get_output("vcf")
+
+        cmd = "%s norm" % bcftools
+        if split_multiallelic:
+            # Optionally specify to split multiallelic into two lines
+            cmd += " -m-both"
+        cmd += " -f %s -o %s %s" % (ref, vcf_out, vcf_in)
+
+        # Capture stderr
+        cmd += " !LOG3!"
+
+        # Return cmd
+        return cmd
