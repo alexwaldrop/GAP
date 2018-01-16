fd31d7ff8d61dcd16807e1d492748f198397c7c5
Alex Waldrop <amw117@duke.edu>
2018-01-16 12:01:17 -0500

    Updated ParseSamtoolsDepth to reflect changes in QCParser.

--- Modules/Tools/ParseSamtoolsDepth.py
+++ Modules/Tools/ParseSamtoolsDepth.py
@@ -1,17 +1,18 @@
 from Modules import Module
 
-class SummarizeSamtoolsDepth(Module):
+class ParseSamtoolsDepth(Module):
 
     def __init__(self, module_id):
-        super(SummarizeSamtoolsDepth, self).__init__(module_id)
+        super(ParseSamtoolsDepth, self).__init__(module_id)
 
         self.input_keys     = ["samtools_depth", "qc_parser", "nr_cpus", "mem"]
-        self.output_keys    = ["summary_file"]
+        self.output_keys    = ["qc_report"]
 
         self.quick_command  = True
 
     def define_input(self):
         self.add_argument("samtools_depth",     is_required=True)
+        self.add_argument("sample_name",        is_required=True)
         self.add_argument("qc_parser",          is_required=True, is_resource=True)
         self.add_argument("nr_cpus",            is_required=True, default_value=1)
         self.add_argument("mem",                is_required=True, default_value=12)
@@ -19,19 +20,20 @@ class SummarizeSamtoolsDepth(Module):
 
     def define_output(self, platform, split_name=None):
         # Declare output summary filename
-        summary_file = self.generate_unique_file_name(split_name=split_name, extension=".depth.summary.txt")
-        self.add_output(platform, "summary_file", summary_file)
+        summary_file = self.generate_unique_file_name(split_name=split_name, extension=".depth.qc_report.txt")
+        self.add_output(platform, "qc_report", summary_file)
 
     def define_command(self, platform):
 
         # Get options from kwargs
         input_file      = self.get_arguments("samtools_depth").get_value()
         qc_parser       = self.get_arguments("qc_parser").get_value()
+        sample_name     = self.get_arguments("sample_name").get_value()
         cutoffs         = self.get_arguments("depth_cutoffs").get_value()
-        summary_file    = self.get_output("summary_file")
+        qc_report       = self.get_output("qc_report")
 
         # Generating command to parse samtools depth output
-        cmd = "%s coverage -i %s" % (qc_parser, input_file)
+        cmd = "%s SamtoolsDepth -i %s -s %s " % (qc_parser, input_file, sample_name)
 
         # Add options for coverage depth cutoffs to report
         for cutoff in cutoffs:
@@ -39,5 +41,5 @@ class SummarizeSamtoolsDepth(Module):
             cmd += " --ct %d" % cutoff
 
         # Write output to summary file
-        cmd += " > %s !LOG2!" % summary_file
+        cmd += " > %s !LOG2!" % qc_report
         return cmd
