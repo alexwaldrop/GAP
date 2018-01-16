d9c2c6665f662da074641aba6cc28d21599bf82a
Alex Waldrop <amw117@duke.edu>
2018-01-16 12:04:30 -0500

    Updated ParseSamtoolsFlagstat to reflect QCParser changes.

--- Modules/Tools/ParseSamtoolsFlagstat.py
+++ Modules/Tools/ParseSamtoolsFlagstat.py
@@ -1,34 +1,36 @@
 from Modules import Module
 
-class SummarizeSamtoolsFlagstat(Module):
+class ParseSamtoolsFlagstat(Module):
 
     def __init__(self, module_id):
-        super(SummarizeSamtoolsFlagstat, self).__init__(module_id)
+        super(ParseSamtoolsFlagstat, self).__init__(module_id)
 
         self.input_keys     = ["flagstat", "qc_parser", "nr_cpus", "mem"]
-        self.output_keys    = ["summary_file"]
+        self.output_keys    = ["qc_report"]
 
         # Command should be run on main processor
         self.quick_command = True
 
     def define_input(self):
-        self.add_argument("flagstat",   is_required=True)
-        self.add_argument("qc_parser",  is_required=True, is_resource=True)
-        self.add_argument("nr_cpus",    is_required=True, default_value=1)
-        self.add_argument("mem",        is_required=True, default_value=1)
+        self.add_argument("flagstat",       is_required=True)
+        self.add_argument("sample_name",    is_required=True)
+        self.add_argument("qc_parser",      is_required=True, is_resource=True)
+        self.add_argument("nr_cpus",        is_required=True, default_value=1)
+        self.add_argument("mem",            is_required=True, default_value=1)
 
     def define_output(self, platform, split_name=None):
         # Declare output summary filename
-        summary_file = self.generate_unique_file_name(split_name=split_name, extension=".flagstat.summary.txt")
-        self.add_output(platform, "summary_file", summary_file)
+        summary_file = self.generate_unique_file_name(split_name=split_name, extension=".flagstat.qc_report.txt")
+        self.add_output(platform, "qc_report", summary_file)
 
     def define_command(self, platform):
 
         # Get options from kwargs
         input_file      = self.get_arguments("flagstat").get_value()
         qc_parser       = self.get_arguments("qc_parser").get_value()
-        summary_file    = self.get_output("summary_file")
+        sample_name     = self.get_arguments("sample_name").get_value()
+        qc_report       = self.get_output("qc_report")
 
         # Generating command to parse samtools flagstat output
-        cmd = "%s flagstat -i %s > %s !LOG2!" % (qc_parser, input_file, summary_file)
+        cmd = "%s SamtoolsFlagstat -i %s -s %s > %s !LOG2!" % (qc_parser, input_file, sample_name, qc_report)
         return cmd
