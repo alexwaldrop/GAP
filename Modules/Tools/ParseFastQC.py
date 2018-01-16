9f2acc7a7bbc2932dc420eb8f143e48cbe0f2952
Alex Waldrop <amw117@duke.edu>
2018-01-16 12:16:37 -0500

    Update ParseFastQC to reflect QCParser update.

--- Modules/Tools/ParseFastQC.py
+++ Modules/Tools/ParseFastQC.py
@@ -2,60 +2,60 @@ import os
 
 from Modules import Module
 
-class SummarizeFastQC(Module):
+class ParseFastQC(Module):
 
     def __init__(self, module_id):
-        super(SummarizeFastQC, self).__init__(module_id)
+        super(ParseFastQC, self).__init__(module_id)
 
         self.input_keys     = ["R1_fastqc", "R2_fastqc", "qc_parser", "nr_cpus", "mem"]
-        self.output_keys    = ["summary_file"]
+        self.output_keys    = ["qc_report"]
 
         # Command should be run on main processor
         self.quick_command = True
 
     def define_input(self):
-        self.add_argument("R1_fastqc",  is_required=True)
-        self.add_argument("R2_fastqc",  is_required=False)
-        self.add_argument("qc_parser",  is_required=True, is_resource=True)
-        self.add_argument("nr_cpus",    is_required=True, default_value=1)
-        self.add_argument("mem",        is_required=True, default_value=1)
-        self.add_argument("fastq_type")
+        self.add_argument("R1_fastqc",      is_required=True)
+        self.add_argument("R2_fastqc",      is_required=False)
+        self.add_argument("sample_name",    is_required=True)
+        self.add_argument("qc_parser",      is_required=True, is_resource=True)
+        self.add_argument("nr_cpus",        is_required=True, default_value=1)
+        self.add_argument("mem",            is_required=True, default_value=1)
 
     def define_output(self, platform, split_name=None):
-        summary_file = self.generate_unique_file_name(split_name=split_name, extension=".fastqc.summary.txt")
-        self.add_output(platform, "summary_file", summary_file)
+        summary_file = self.generate_unique_file_name(split_name=split_name, extension=".fastqc.qc_report.txt")
+        self.add_output(platform, "qc_report", summary_file)
 
     def define_command(self, platform):
         # Get options from kwargs
         r1_fastqc_dir       = self.get_arguments("R1_fastqc").get_value()
         r2_fastqc_dir       = self.get_arguments("R2_fastqc").get_value()
-        fastq_type          = self.get_arguments("fastq_type").get_value()
         qc_parser           = self.get_arguments("qc_parser").get_value()
-        summary_file        = self.get_output("summary_file")
+        sample_name         = self.get_arguments("sample_name").get_value()
+        qc_report           = self.get_output("qc_report")
 
         # Get command for parsing R1 fastqc output
-        r1_column_header = "R1_%s" % fastq_type if fastq_type is not None else None
-        r1_parse_cmd, r1_output = self.__get_one_fastqc_cmd(r1_fastqc_dir,
-                                                            qc_parser,
-                                                            column_header_suffix=r1_column_header)
+        r1_parse_cmd, r1_output = self.__get_one_fastqc_cmd(r1_fastqc_dir, qc_parser, sample_name)
 
-        # cmd to summarize only R1 fastqc output and cat to correct output filename
-        cmd = "%s ; cat %s > %s" % (r1_parse_cmd, r1_output, summary_file)
+        if r2_fastqc_dir is None:
+            # Case: No R1 provided
+            cmd = "%s ; cat %s > %s" % (r1_parse_cmd, r1_output, qc_report)
 
-        # Conditionally append command for parsing R2 fastqc output
-        if r2_fastqc_dir is not None:
-            r2_column_header = "R2_%s" % fastq_type if fastq_type is not None else None
-            r2_parse_cmd, r2_output = self.__get_one_fastqc_cmd(r2_fastqc_dir,
-                                                                qc_parser,
-                                                                column_header_suffix=r2_column_header)
+        else:
+            # Case: R2 provided
+
+            # Generate QCReport for R2 fastqc
+            r2_parse_cmd, r2_output = self.__get_one_fastqc_cmd(r2_fastqc_dir, qc_parser, sample_name)
+
+            # Rbind R1 and R2 QCReports into a single report
+            rbind_cmd = "%s Rbind -i %s %s > %s !LOG2!" % (qc_parser, r1_output, r2_output, qc_report)
 
             # cmd to summarize R1 and R2 and paste together into a single output file
-            cmd = "%s ; %s ; paste %s %s > %s !LOG2!" % (r1_parse_cmd, r2_parse_cmd,
-                                                         r1_output, r2_output, summary_file)
+            cmd = "%s ; %s ; %s" % (r1_parse_cmd, r2_parse_cmd, rbind_cmd)
+
         return cmd
 
     @staticmethod
-    def __get_one_fastqc_cmd(fastqc_dir, qc_parser, column_header_suffix=None):
+    def __get_one_fastqc_cmd(fastqc_dir, qc_parser, sample_name):
         # Get command for summarizing output from fastqc
 
         # Get input filename
@@ -64,12 +64,5 @@ class SummarizeFastQC(Module):
         # Get output filename
         output = "%s.fastqcsummary.txt" % fastqc_summary_file.split("_fastqc")[0]
 
-        cmd = "%s fastqc -i %s" % (qc_parser, fastqc_summary_file)
-
-        # Optionally add a string to column headers in output table
-        cmd += " -p %s" % column_header_suffix if column_header_suffix is not None else ""
-
-        # Write to output file and log errors
-        cmd += " > %s !LOG2!" % output
-
+        cmd = "%s FastQC -i %s -s %s > %s !LOG2!" % (qc_parser, fastqc_summary_file, sample_name, output)
         return cmd, output
