9dc1b7214817edd9acd3877049da2b0bf1608477
Alex Waldrop <amw117@duke.edu>
2018-01-25 10:24:08 -0500

    Updated CellRanger module.
    Now accepts 'R1', 'R2' as inputs instead of R1_fastqs, R2_fastqs.

--- Modules/Tools/CellRanger.py
+++ Modules/Tools/CellRanger.py
@@ -6,20 +6,20 @@ class CellRanger(Module):
     def __init__(self, module_id):
         super(CellRanger, self).__init__(module_id)
 
-        self.input_keys = ["sample_name", "R1_fastqs", "R2_fastqs",
+        self.input_keys = ["sample_name", "R1", "R2",
                            "cellranger", "singlecell_rnaseq_ref",
                            "nr_cpus", "mem"]
 
         self.output_keys = ["cellranger_output_dir"]
 
     def define_input(self):
-        self.add_argument("sample_name",                is_required=True)
-        self.add_argument("R1_fastqs",                  is_required=True)
-        self.add_argument("R2_fastqs",                  is_required=True)
-        self.add_argument("cellranger",                 is_required=True, is_resource=True)
-        self.add_argument("singlecell_rnaseq_ref",      is_required=True, is_resource=True)
-        self.add_argument("nr_cpus",                    is_required=True, default_value=12)
-        self.add_argument("mem",                        is_required=True, default_value=48)
+        self.add_argument("sample_name",            is_required=True)
+        self.add_argument("R1",                     is_required=True)
+        self.add_argument("R2",                     is_required=True)
+        self.add_argument("cellranger",             is_required=True, is_resource=True)
+        self.add_argument("singlecell_rnaseq_ref",  is_required=True, is_resource=True)
+        self.add_argument("nr_cpus",                is_required=True, default_value=12)
+        self.add_argument("mem",                    is_required=True, default_value=48)
 
     def define_output(self, platform, split_name=None):
         # Declare cell ranger output dir
@@ -30,8 +30,8 @@ class CellRanger(Module):
         # Generate command for running Cell Ranger
         cellranger      = self.get_arguments("cellranger").get_value()
         sample_name     = self.get_arguments("sample_name").get_value()
-        R1_fastqs       = self.get_arguments("R1_fastqs").get_value()
-        R2_fastqs       = self.get_arguments("R2_fastqs").get_value()
+        R1              = self.get_arguments("R1").get_value()
+        R2              = self.get_arguments("R2").get_value()
         transcriptome   = self.get_arguments("singlecell_rnaseq_ref").get_value()
         nr_cpus         = self.get_arguments("nr_cpus").get_value()
         mem             = self.get_arguments("mem").get_value()
@@ -45,20 +45,26 @@ class CellRanger(Module):
         if not fastq_dir.endswith("/"):
             fastq_dir += "/"
 
-        mv_R1_cmd = ""
-        mv_R2_cmd = ""
-        for i in range(len(R1_fastqs)):
-            new_R1 = os.path.join(fastq_dir, "sample_S0_L000_R1_00{}.fastq.gz".format(i))
-            new_R2 = os.path.join(fastq_dir, "sample_S0_L000_R2_00{}.fastq.gz".format(i))
-            mv_R1_cmd += "mv {0} {1};".format(R1_fastqs[i], new_R1)
-            mv_R2_cmd += "mv {0} {1};".format(R2_fastqs[i], new_R2)
-
+        # Coerce R1, R2 to lists even if they're single files
+        # This means R1, R2 can be list of fastq files or a single fastq
+        if not isinstance(R1, list):
+            R1 = [R1]
+            R2 = [R2]
 
         # We accommodate three idiosynchroses of Cell Ranger:
         # 1. CR accepts a folder of fastqs, not a list of files,
         #    so we move the fastqs to fastq_dir before calling Cell Ranger
         # 2. CR only accepts certain formats of fastq file names,
         #    so we rename the fastq files when moving
+        mv_R1_cmd = ""
+        mv_R2_cmd = ""
+        for i in range(len(R1)):
+            new_R1 = os.path.join(fastq_dir, "sample_S0_L000_R1_00{}.fastq.gz".format(i))
+            new_R2 = os.path.join(fastq_dir, "sample_S0_L000_R2_00{}.fastq.gz".format(i))
+            mv_R1_cmd += "mv {0} {1};".format(R1, new_R1)
+            mv_R2_cmd += "mv {0} {1};".format(R2, new_R2)
+
+
         # 3. If interrupted, the lock file needs to be removed before restarting,
         #    so we remove the lock file just in case it exists
         cmd = "cd {0}; source {1}; rm -f {0}{5}/_lock; mkdir {2}; {3} {4} " \
