1a9caf7b6693114ce03036eb94bab9c534348ad1
Razvan Panea <razvan.panea@duke.edu>
2018-02-22 12:56:30 -0500

    Modifies VCFMerger to increase the usage resources

--- Modules/Mergers/VCFMerger.py
+++ Modules/Mergers/VCFMerger.py
@@ -7,14 +7,13 @@ class VCFMerger(Module):
 
         self.input_keys     = ["vcf", "snpsift", "java", "nr_cpus", "mem"]
         self.output_keys    = ["vcf"]
-        self.quick_command  = True
 
     def define_input(self):
         self.add_argument("vcf",                is_required=True)
         self.add_argument("snpsift",            is_required=True, is_resource=True)
         self.add_argument("java",               is_required=True, is_resource=True)
-        self.add_argument("nr_cpus",            is_required=True, default_value=1)
-        self.add_argument("mem",                is_required=True, default_value=1)
+        self.add_argument("nr_cpus",            is_required=True, default_value=8)
+        self.add_argument("mem",                is_required=True, default_value=40)
 
     def define_output(self, platform, split_name=None):
         # Declare name of merged VCF output file
@@ -26,8 +25,12 @@ class VCFMerger(Module):
         vcf_list    = self.get_arguments("vcf").get_value()
         snpsift     = self.get_arguments("snpsift").get_value()
         java        = self.get_arguments("java").get_value()
+        mem         = self.get_arguments("mem").get_value()
         vcf_out     = self.get_output("vcf")
 
+        # Generating JVM options
+        jvm_options = "-Xmx%dG -Djava.io.tmpdir=%s" % (mem * 9 / 10, platform.get_workspace_dir("tmp"))
+
         # Generating SnpEff command
-        cmd = "%s -jar %s sort %s > %s !LOG2!" % (java, snpsift, " ".join(vcf_list), vcf_out)
+        cmd = "%s %s -jar %s sort %s > %s !LOG2!" % (java, jvm_options, snpsift, " ".join(vcf_list), vcf_out)
         return cmd
