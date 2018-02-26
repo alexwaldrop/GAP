bdd7086a30b0378c8b0d6cced395f89d4653ca39
Razvan Panea <razvan.panea@duke.edu>
2018-02-26 14:27:27 -0500

    Implements MuTect2 module from GATK3.7

---
+++ Modules/Tools/GATKMuTect2.py
@@ -0,0 +1,86 @@
+from Modules import Module
+
+class GATKMuTect2(Module):
+
+    def __init__(self, module_id):
+        super(GATKMuTect2, self).__init__(module_id)
+
+        self.input_keys = ["bam", "bam_idx", "is_tumor",
+                           "location", "excluded_location",
+                           "gatk", "java", "ref", "dbsnp", "cosmic",
+                           "nr_cpus", "mem"]
+
+        self.output_keys = ["vcf", "vcf_idx"]
+
+    def define_input(self):
+        self.add_argument("bam",                is_required=True)
+        self.add_argument("bam_idx",            is_required=True)
+        self.add_argument("is_tumor",           is_required=True)
+        self.add_argument("gatk",               is_required=True, is_resource=True)
+        self.add_argument("java",               is_required=True, is_resource=True)
+        self.add_argument("ref",                is_required=True, is_resource=True)
+        self.add_argument("dbsnp",              is_required=True, is_resource=True)
+        self.add_argument("cosmic",             is_required=True, is_resource=True)
+        self.add_argument("nr_cpus",            is_required=True, default_value=2)
+        self.add_argument("mem",                is_required=True, default_value="nr_cpus * 3.5")
+        self.add_argument("location")
+        self.add_argument("excluded_location")
+
+    def define_output(self, platform, split_name=None):
+        # Declare VCF output filename
+        vcf = self.generate_unique_file_name(split_name=split_name, extension=".vcf")
+        self.add_output(platform, "vcf", vcf)
+        # Declare VCF index output filename
+        vcf_idx = self.generate_unique_file_name(split_name=split_name, extension=".vcf.idx")
+        self.add_output(platform, "vcf_idx", vcf_idx)
+
+    def define_command(self, platform):
+        # Get input arguments
+        bam     = self.get_arguments("bam").get_value()
+        is_tumor= self.get_arguments("is_tumor").get_value()
+        gatk    = self.get_arguments("gatk").get_value()
+        java    = self.get_arguments("java").get_value()
+        ref     = self.get_arguments("ref").get_value()
+        dbsnp   = self.get_arguments("dbsnp").get_value()
+        cosmic  = self.get_arguments("cosmic").get_value()
+        L       = self.get_arguments("location").get_value()
+        XL      = self.get_arguments("excluded_location").get_value()
+        mem     = self.get_arguments("mem").get_value()
+
+        # Get output file
+        vcf    = self.get_output("vcf")
+
+        # Set JVM options
+        jvm_options = "-Xmx%dG -Djava.io.tmpdir=%s" % (mem * 4 / 5, platform.get_workspace_dir("tmp"))
+
+        # Generating the MuTect2 options
+        opts = list()
+        if is_tumor[0]:
+            opts.append("-I:tumor %s" % bam[0])
+            opts.append("-I:normal %s" % bam[1])
+        else:
+            opts.append("-I:tumor %s" % bam[1])
+            opts.append("-I:normal %s" % bam[0])
+        opts.append("-o %s" % vcf)
+        opts.append("-R %s" % ref)
+        opts.append("--dbsnp %s" % dbsnp)
+        opts.append("--cosmic %s" % cosmic)
+
+        # Limit the locations to be processes
+        if L is not None:
+            if isinstance(L, list):
+                for included in L:
+                    if included != "unmapped":
+                        opts.append("-L \"%s\"" % included)
+            else:
+                opts.append("-L \"%s\"" % L)
+        if XL is not None:
+            if isinstance(XL, list):
+                for excluded in XL:
+                    opts.append("-XL \"%s\"" % excluded)
+            else:
+                opts.append("-XL \"%s\"" % XL)
+
+        # Generating command for base recalibration
+        cmd = "%s %s -jar %s -T MuTect2 %s !LOG3!" % (java, jvm_options, gatk, " ".join(opts))
+        return cmd
