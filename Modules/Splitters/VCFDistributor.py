8a8270409a56da881e3cf9dda70ba7b98ff317c2
Alex Waldrop <amw117@duke.edu>
2018-03-02 23:20:34 -0500

    Add VCFDistributor module.

--- Modules/Splitters/VCFDistributor.py
+++ Modules/Splitters/VCFDistributor.py
@@ -1,32 +1,24 @@
 from Modules import Module
 
-class GVCFDistributor(Module):
+class VCFDistributor(Module):
 
     def __init__(self, module_id):
-        super(GVCFDistributor, self).__init__(module_id)
+        super(VCFDistributor, self).__init__(module_id)
 
-        self.input_keys     = ["gvcf", "gvcf_idx", "nr_cpus", "mem"]
-        self.output_keys    = ["gvcf", "gvcf_idx"]
-
+        self.input_keys     = ["vcf", "nr_cpus", "mem"]
+        self.output_keys    = ["vcf"]
         self.quick_command = True
 
     def define_input(self):
-        self.add_argument("gvcf",           is_required=True)
-        self.add_argument("gvcf_idx",       is_required=True)
-        self.add_argument("nr_cpus",        is_required=True, default_value=1)
-        self.add_argument("mem",            is_required=True, default_value=1)
+        self.add_argument("vcf",        is_required=True)
+        self.add_argument("nr_cpus",    is_required=True, default_value=1)
+        self.add_argument("mem",        is_required=True, default_value=1)
 
     def define_output(self, platform, split_name=None):
-
-        gvcfs       = self.get_arguments("gvcf").get_value()
-        gvcfs_idx   = self.get_arguments("gvcf_idx").get_value()
-
-        for count, (gvcf, gvcf_idx) in enumerate(zip(gvcfs, gvcfs_idx)):
-            split_name = "gvcf_%s" % count
-            split_data = {
-                "gvcf": gvcf,
-                "gvcf_idx": gvcf_idx
-            }
+        vcfs       = self.get_arguments("vcf").get_value()
+        for count, vcf in enumerate(vcfs):
+            split_name = "vcf_%s" % count
+            split_data = {"vcf": vcf}
             self.add_output(platform, split_name, split_data, is_path=False)
 
     def define_command(self, platform):
