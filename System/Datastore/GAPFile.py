f99f41ebe571cf1d8e92c5c68e8fc0c015ae3d1e
Alex Waldrop <amw117@duke.edu>
2018-05-04 19:31:40 -0400

    Updated GAPFile object to encapsulate functionality of GAPObject.

--- System/Datastore/GAPFile.py
+++ System/Datastore/GAPFile.py
@@ -1,22 +1,32 @@
 import os
-import GAPObject
+import logging
 
-class GAPFile (GAPObject):
-    # GAPObject specifically related to files
-    def __init__(self, obj_type, name, val, **kwargs):
+class GAPFileMetadataError(Exception):
+    # Base class for exception related to trying to access unavailable file metadata
+    pass
 
-        # Inherits from GAPObject
-        super(GAPFile, self).__init__(obj_type, name, val)
+class GAPFile:
+    # Hold GAP-Related file information
+    def __init__(self, file_id,  file_type, path, **kwargs):
+
+        # File Id
+        self.file_id = file_id
+
+        # Object type e.g. 'samtools', 'ref'
+        self.type = file_type
+
+        # Object data
+        self.path = path
 
         # Check to make sure value is string (Path)
-        assert isinstance(self.value, basestring), "GAPFile value must be string! Recieved '%s' of type '%s'" % (val, type(val))
+        assert isinstance(self.path, basestring), "GAPFile value must be string! Recieved '%s' of type '%s'" % (path, type(path))
 
         # Path to a containing directory where resource is found
         self.containing_dir = kwargs.pop("containing_dir", None)
 
         # Boolean flag for whether path refers to an actual file or is a basename for a set of files
         # E.g. BWA index files may be listed as /path/to/bwa_index and refer to a set of files sharing a common suffix
-        self.__is_prefix = self.value.endswith("*")
+        self.__is_prefix = self.path.endswith("*")
 
         # Boolean for whether resource is an executable
         self.__is_executable = kwargs.pop("export_to_path", False)
@@ -30,18 +40,27 @@ class GAPFile (GAPObject):
         # Standardize aspects of the resource path provided
         self.__standardize()
 
-    def __standardize(self):
+        # Metadata associated with an object
+        self.metadata = kwargs
+
+    def get_file_id(self):
+        return self.file_id
+
+    def get_path(self):
+        return self.path
+
+    def get_type(self):
+        return self.type
+
+    @property
+    def filename(self):
+        return self.path.rstrip("/").split("/")[-1]
+
+    @property
+    def containing_dir_name(self):
         if self.containing_dir is not None:
-            # Standardize file path of containing directory
-            self.containing_dir = self.containing_dir.rstrip("/") + "/"
-
-            # Make all path absolute from containing directory
-            file_name = self.value.replace(self.containing_dir, "")
-            self.value = os.path.join(self.containing_dir, file_name)
-
-        if self.is_prefix():
-            # Remove wildcard character from path is path is prefix
-            self.value = self.value.replace("*", "")
+            return self.containing_dir.rstrip("/").split("/")[-1]
+        return None
 
     def get_containing_dir(self):
         return self.containing_dir
@@ -53,7 +72,7 @@ class GAPFile (GAPObject):
         return self.__is_prefix
 
     def is_remote(self):
-        return ":" in self.value if self.containing_dir is None else ":" in self.containing_dir
+        return ":" in self.path if self.containing_dir is None else ":" in self.containing_dir
 
     def is_executable(self):
         return self.__is_executable
@@ -65,20 +84,65 @@ class GAPFile (GAPObject):
         # Return true if size is known
         return self.size is not None
 
-    def set_containing_dir(self, new_containing_dir):
-        self.value           = self.value.replace(self.containing_dir, "")
-        self.containing_dir = new_containing_dir
+    def set_size(self, file_size):
+        # Set file size (GB)
+        self.size = file_size
+
+    def has_metadata_type(self, meta_type):
+        return meta_type in self.metadata
+
+    def get_metadata(self, meta_type):
+        if not meta_type in self.metadata:
+            logging.error("GAPObject '%s' of type '%s' doesn't have metadata of type '%s'" % (self.file_id, self.type, meta_type))
+            raise GAPFileMetadataError("GAPObject does not have metadata of type '%s'" % meta_type)
+        return self.metadata[meta_type]
+
+    def set_metadata(self, meta_type, val):
+        self.metadata[meta_type] = val
+
+    def set_path(self, new_path):
+        self.path = new_path
+
+    def update_path(self, dest_dir):
+        # Updates path assuming file has been moved to a new directory
+        if self.containing_dir is not None:
+            # Assume file has been moved inside containing dir
+            self.__update_containing_dir(dest_dir)
+        else:
+            self.path = os.path.join(dest_dir, self.filename)
+
+    def __update_containing_dir(self, dest_dir):
+        # Updates path assuming entire containing directory has been moved to a new directory
+        new_path = os.path.join(dest_dir, self.containing_dir_name)
+        self.path = self.path.replace(self.containing_dir, "")
+        self.containing_dir = new_path
         self.__standardize()
+
+    def __standardize(self):
+        if self.containing_dir is not None:
+            # Standardize file path of containing directory
+            self.containing_dir = self.containing_dir.rstrip("/") + "/"
+
+            # Make all path absolute from containing directory
+            file_name = self.path.replace(self.containing_dir, "")
+            self.path = os.path.join(self.containing_dir, file_name)
+
+        if self.is_prefix():
+            # Remove wildcard character from path is path is prefix
+            self.path = self.path.replace("*", "")
 
     def __str__(self):
         to_return = "=============\n"
-        to_return += "Name:\t%s\n" % self.name
+        to_return += "FileID:\t%s\n" % self.file_id
         to_return += "Type:\t%s\n" % self.type
-        to_return += "Path:\t%s\n" % self.value
+        to_return += "Path:\t%s\n" % self.path
         to_return += "Containing_dir:\t%s\n" % self.containing_dir
         to_return += "is_remote:\t%s\n" % self.is_remote()
         to_return += "is_executable:\t%s\n" % self.__is_executable
         to_return += "is_library:\t%s\n" % self.__is_library
         to_return += "is_prefix:\t%s\n" % self.__is_prefix
+        to_return += "size:\t%s\n" % self.size
         to_return += "=============\n"
         return to_return
+
+
