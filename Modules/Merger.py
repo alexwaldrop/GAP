0bbedb600ada5a9b042e89689a67633ae6615c7e
Alex Waldrop <amw117@duke.edu>
2018-05-08 17:17:55 -0400

    Created base Merger class.

---
+++ Modules/Merger.py
@@ -0,0 +1,19 @@
+from Module import Module
+
+class Merger(Module):
+
+    def __init__(self, module_id):
+        super(Merger, self).__init__(module_id)
+        self.merger = True
+
+    def define_input(self):
+        raise NotImplementedError(
+            "Merger module %s must implement 'define_input()' function!" % self.__class__.__name__)
+
+    def define_output(self):
+        raise NotImplementedError(
+            "Merger module %s must implement 'define_output()' function!" % self.__class__.__name__)
+
+    def define_command(self):
+        raise NotImplementedError(
+            "Merger module %s must implement 'define_command()' function!" % self.__class__.__name__)
