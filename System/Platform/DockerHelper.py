5ae8fcbd18cd0358791b053f3d9944e806d2f8ed
Alex Waldrop <amw117@duke.edu>
2018-05-21 12:13:32 -0400

    Added DockerHelper to take care of docker-related functions.

---
+++ System/Platform/DockerHelper.py
@@ -0,0 +1,63 @@
+import logging
+
+from TaskPlatform import TaskPlatform
+
+class DockerHelper(object):
+    # Class designed to facilitate remote file manipulations for a processor
+
+    def __init__(self, proc):
+        self.proc = proc
+
+    def pull(self, image_name, job_name=None, log=True, num_retries=0):
+        # Pull docker image on local processor
+
+        cmd = "sudo docker pull %s" % image_name
+
+        job_name = "pull_%s" % image_name if job_name is None else job_name
+
+        # Optionally add logging
+        cmd = "%s !LOG3!" % cmd if log else cmd
+
+        # Run command and return job name
+        self.proc.run(job_name, cmd, num_retries=num_retries)
+        return job_name
+
+    def image_exists(self, image_name, job_name=None, num_retries=0):
+        # Return true if file exists, false otherwise
+
+        # Run command and return job name
+        job_name = "check_exists_%s" % image_name if job_name is None else job_name
+
+        # Wait for cmd to finish and get output
+        try:
+            self.pull(image_name, job_name, log=False, num_retries=num_retries)
+            out, err = self.proc.wait_process(job_name)
+            return len(err) == 0
+        except RuntimeError:
+            return False
+        except:
+            logging.error("Unable to check docker image existence: %s" % image_name)
+            raise
+
+    def get_image_size(self, image_name, job_name=None, num_retries=0):
+        # Return file size in gigabytes
+        cmd = "sudo docker image inspect %s --format='{{.Size}}'" % image_name
+
+        # Run command and return job name
+        job_name = "get_size_%s" % image_name if job_name is None else job_name
+        self.proc.run(job_name, cmd, num_retries=num_retries)
+
+        # Wait for cmd to finish and get output
+        try:
+            # Try to return file size in gigabytes
+            out, err = self.proc.wait_process(job_name)
+            # Iterate over all files if multiple files (can happen if wildcard)
+            bytes = [int(x.split()[0]) for x in out.split("\n") if x != ""]
+            # Add them up and divide by billion bytes
+            return sum(bytes)/(1024**3.0)
+
+        except BaseException, e:
+            logging.error("Unable to check docker image size: %s" % image_name)
+            if e.message != "":
+                logging.error("Received the following msg:\n%s" % e.message)
+            raise
