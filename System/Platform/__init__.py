ba9180c946a6d068bdeded928b120f81bc3c34ab
Alex Waldrop <amw117@duke.edu>
2018-05-22 15:23:35 -0400

    Platform __init__ imports TaskProcessor.

--- System/Platform/__init__.py
+++ System/Platform/__init__.py
@@ -2,6 +2,7 @@ from Process import Process
 from Processor import Processor
 from Platform import Platform
 from TaskPlatform import TaskPlatform
+from TaskProcessor import TaskProcessor
 from StorageHelper import StorageHelper
 from DockerHelper import DockerHelper
 
