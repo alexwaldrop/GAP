2ba8c50b83810beea477e1286fa300021dc55727
Alex Waldrop <amw117@duke.edu>
2018-05-24 11:45:25 -0400

    Updated SamtoolsIndex to new Module type.

--- Modules/Tools/SamtoolsIndex.py
+++ Modules/Tools/SamtoolsIndex.py
@@ -3,12 +3,7 @@ from Modules import Module
 class SamtoolsIndex(Module):
     def __init__(self, module_id):
         super(SamtoolsIndex, self).__init__(module_id)
-
-        self.input_keys = ["bam", "samtools", "nr_cpus", "mem"]
         self.output_keys = ["bam_idx"]
-
-        # Command should be run on main processor
-        self.quick_command = True
 
     def define_input(self):
         self.add_argument("bam",        is_required=True)
@@ -16,10 +11,10 @@ class SamtoolsIndex(Module):
         self.add_argument("nr_cpus",    is_required=True, default_value=1)
         self.add_argument("mem",        is_required=True, default_value=1)
 
-    def define_output(self, platform, split_name=None):
+    def define_output(self):
 
         # Get arguments value
-        bams = self.get_arguments("bam").get_value()
+        bams = self.get_argument("bam")
 
         # Check if the input is a list
         if isinstance(bams, list):
@@ -28,12 +23,12 @@ class SamtoolsIndex(Module):
             bams_idx = bams + ".bai"
 
         # Add new bams as output
-        self.add_output(platform, "bam_idx", bams_idx, is_path=False)
+        self.add_output("bam_idx", bams_idx, is_path=True)
 
-    def define_command(self, platform):
+    def define_command(self):
         # Define command for running samtools index from a platform
-        bam         = self.get_arguments("bam").get_value()
-        samtools    = self.get_arguments("samtools").get_value()
+        bam         = self.get_argument("bam")
+        samtools    = self.get_argument("samtools")
         bam_idx     = self.get_output("bam_idx")
 
         # Generating indexing command
