75e12e5d8e76633f5dd19c116e29b99aaae6ea92
Alex Waldrop <amw117@duke.edu>
2018-05-24 17:59:37 -0400

    Added temporary test functions.

--- Test.py
+++ Test.py
@@ -1,49 +1,127 @@
+import logging
 from System.Graph import Graph
 from Main import configure_import_paths
+import time
+import importlib
 
-######################### Graph Tests
 configure_import_paths()
+
+
+
+######################### Workspace tests
+from System.Datastore.Datastore import TaskWorkspace
+
+wrkspace = TaskWorkspace("/home/alex/wrk", "gs://davelab_tmp/123", "gs://davelab_final/123")
+print wrkspace.debug_string()
+
+exit(0)
+
+
+######################### Module tests
+def load_module(module_name, module_id):
+    # Try importing the module
+    module = importlib.import_module(module_name)
+    # Get the class
+    _class = module.__dict__[module_name]
+    return _class(module_id)
+
+
+sam_index = load_module("SamtoolsFlagstat", "samflagstat")
+sam_index.set_argument("samtools", "/home/samtools")
+sam_index.set_argument("bam", "/home/test.bam")
+sam_index.set_argument("bam_idx", "/home/test.bam.bai")
+
+print sam_index.get_output_values()
+print sam_index.get_input_values()
+
+print sam_index.generate_unique_file_name()
+print sam_index.generate_unique_file_name(extension=".txt.fuck.txt")
+print sam_index.set_output_dir("/home/new/output")
+print sam_index.generate_unique_file_name(extension=".txt.fuck.txt")
+print sam_index.get_command()
+
+fastq_splitter = load_module("FastqSplitter", "splitdatfastq")
+fastq_splitter.set_output_dir("/home/dis/a/output")
+
+fastq_splitter.set_argument("max_nr_cpus", 32)
+fastq_splitter.set_argument("R1", "/data/r1.fastq")
+#fastq_splitter.set_argument("R2", "/data/r2.fastq")
+fastq_splitter.set_argument("nr_reads", 10000000)
+fastq_splitter.set_argument("read_len", 200)
+print fastq_splitter.get_command()
+
+print fastq_splitter.get_output_values()
+print len(fastq_splitter.get_output_values())
+print fastq_splitter.get_output()
+for output in fastq_splitter.get_output_values():
+    print output
+
+exit(0)
+########################################
 graph_file = "/home/alex/Desktop/projects/sad_monkey/gap/RNAseq/upstream/mmul8_RNAseq_PE_upstream_graph.config"
+rk_file = "/home/alex/Desktop/projects/gap/rk.config"
 
+from System.Datastore.ResourceKit import ResourceKit
 
-graph = Graph(graph_file)
+rk = ResourceKit(rk_file)
 
-print graph.get_nodes()
-print graph.get_adjacency_list()
+print rk.get_resources("samtools")
 
-print
-print
+for docker_id, docker in rk.get_docker_images().iteritems():
+    print docker.get_image_name()
+    for file_type, file_data in docker.get_resources().iteritems():
+        print "File type: %s" % file_type
+        for file_id, gap_file in file_data.iteritems():
+            print gap_file
+            print gap_file.debug_string()
+            print gap_file.is_flagged("docker")
+exit(0)
+print "Samtools: %s" % rk.has_resource_type("samtools")
+print "Derp: %s" % rk.has_resource_type("derp")
+print "hamtools docker: %s" % rk.has_docker_image("hamtools")
+print "hamtools has samtools: %s" % rk.get_docker_images("hamtools").get_resources("samtools")
+exit(0)
 
-new_node = graph.get_nodes()["tool13_RSEM"].split("BURP")
-graph.add_node(new_node)
-print graph.get_nodes()
-print graph.get_adjacency_list()
-
-print graph.get_nodes()["tool13_RSEM"] is graph.get_nodes()["BURP"]
-print
-print
-
-graph.add_edge("BURP", "tool6_FLAGSTAT")
-print graph.get_nodes()
-print graph.get_adjacency_list()
-
-print
-print
-
-#graph.remove_node("tool6_FLAGSTAT")
-#print graph.get_nodes()
-#print graph.get_adjacency_list()
-
-print
-print
-new_graph = graph.get_subgraph(["BURP", "tool7_STAR", "tool18_UTR_OVERLAP"])
-print new_graph.get_nodes()
-print new_graph.get_adjacency_list()
-
-
-print
-print
-print graph.get_dependents("tool2_TRIMMOMATIC")
-print graph.get_dependents("tool6_FLAGSTAT")
 
 ######################### Graph Tests
+
+#src_path = "/home/alex/test.txt"
+#dest_path = "gs://home/alex/dest.txt"
+
+from System.Platform import StorageHelper
+
+from System.Platform.Google import GoogleStandardProcessor
+
+proc = GoogleStandardProcessor("test-proc", 1, 1, zone="us-east1-b",
+                               service_acct="gap-412@davelab-gcloud.iam.gserviceaccount.com",
+                               boot_disk_size=75, disk_image="davelab-image-latest")
+
+proc.set_log_dir("/home/gap/log/")
+proc.create()
+
+print "We READY TO RUN!"
+
+try:
+    sh = StorageHelper(proc)
+    sh.mkdir("/home/alex_waldrop_jr/test/", wait=True)
+    sh.mkdir("/home/gap/log/", wait=True)
+    sh.mkdir("gs://derp_test/mkdir_test_mofo_2/", wait=True)
+    proc.run("perms_gap", "sudo chmod -R 777 /home/gap/")
+    proc.run("perms_awal", "sudo chmod -R 777 /home/alex_waldrop_jr/")
+    proc.wait()
+    print "local exists: %s" % sh.path_exists("/home/alex_waldrop_jr/test/")
+    print "local exists: %s" % sh.path_exists("/home/gap/log/")
+    print "cloud exists: %s" % sh.path_exists("gs://derp_test/mkdir_test_mofo_2/")
+    print "bad exists: %s" % sh.path_exists("/home/aasdasdfk")
+    sh.mv("gs://derp_test/dummy.txt", "/home/alex_waldrop_jr/test/", log=False, wait=True)
+    sh.mv("/home/alex_waldrop_jr/test/dummy.txt", "/home/alex_waldrop_jr/test/whoops_i_win.txt", log=False, wait=True)
+    sh.mv("/home/alex_waldrop_jr/test/whoops_i_win.txt", "gs://derp_test/mkdir_test_mofo_2/", log=False, wait=True)
+    print "good file exists: %s" % sh.path_exists("/home/alex_waldrop_jr/test/dummy.txt")
+    print "DNA file size: %s" % sh.get_file_size("gs://davelab_data/ref/hg19/DNA")
+    print "Dummy file size: %s" % sh.get_file_size("/home/alex_waldrop_jr/test/whoops_i_win.txt")
+
+finally:
+    time.sleep(120)
+    proc.destroy()
+
+
