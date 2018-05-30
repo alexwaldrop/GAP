9177d02f45ec4cf240ff135d9bbb5066cb8a2c73
Alex Waldrop <amw117@duke.edu>
2018-05-30 14:51:44 -0400

    Made SampleSplitter module.

---
+++ Modules/Splitters/SampleSplitter.py
@@ -0,0 +1,22 @@
+from Modules import Splitter
+
+class SampleSplitter(Splitter):
+
+    def __init__(self, module_id):
+        super(SampleSplitter, self).__init__(module_id)
+
+        self.output_keys = ["sample_name"]
+
+    def define_input(self):
+        self.add_argument("sample_name",    is_required=True)
+        self.add_argument("nr_cpus",        is_required=True,   default_value=1)
+        self.add_argument("mem",            is_required=True,   default_value=1)
+
+    def define_output(self):
+        # Obtaining the arguments
+        samples = self.get_argument("sample_name")
+        for sample in samples:
+            self.make_split(split_id=sample, visible_samples=[sample])
+
+    def define_command(self):
+        return None
