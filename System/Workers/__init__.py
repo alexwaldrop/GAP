8fe74b1dfd58da9707de1eb7344435ce8193729d
Alex Waldrop <amw117@duke.edu>
2018-06-01 18:21:45 -0400

    Export PoolWorker and ThreadPool in __init__.

--- System/Workers/__init__.py
+++ System/Workers/__init__.py
@@ -2,3 +2,4 @@ from Thread import Thread
 from ModuleWorker import ModuleWorker
 from NodeWorker import NodeWorker
 from PipelineWorker import PipelineWorker
+from ThreadPool import PoolWorker, ThreadPool
