1e839efc901bacf9e5ab2423510673c89624b9d5
Alex Waldrop <amw117@duke.edu>
2018-06-19 16:17:51 -0400

    Deleted inits for deleted Workers.

--- System/Workers/__init__.py
+++ System/Workers/__init__.py
@@ -1,5 +1,2 @@
 from Thread import Thread
-from ModuleWorker import ModuleWorker
-from NodeWorker import NodeWorker
-from PipelineWorker import PipelineWorker
 from ThreadPool import PoolWorker, ThreadPool
