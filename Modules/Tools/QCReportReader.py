2d6d2899cc21377bf7426c5143ba13c4f81e8608
Alex Waldrop <amw117@duke.edu>
2018-06-21 13:14:24 -0400

    Created QCReportReader module.

--- Modules/Tools/QCReportReader.py
+++ Modules/Tools/QCReportReader.py
@@ -1,9 +1,133 @@
+import logging
+import json
+
 from Modules import Module
 
-class QCReportReader(Module):
+def parse_qc_report(out):
+    # Return QCReport parsed from a string
 
+    # Try loading json
+    try:
+        qc_string = json.loads(out)
+    except:
+        logging.error("Unable to load QCReport! Output is not valid JSON:\n%s" % out)
+        raise
+
+    # Try loading QCReport from json
+    try:
+        qc_report = _QCReport(report=qc_string)
+    except:
+        logging.error("JSON is valid but unable to parse into QCReport!")
+        raise
+    return qc_report
+
+
+class _QCReportError(Exception):
+    pass
+
+
+class _QCReport:
+    # Class ported from QCParser to hold QC data associated with samples, modules
+    def __init__(self, report=None):
+        # Initialize report from existing dictionary or from empty dict
+        self.report     = {} if report is None else report
+        self.validate()
+
+    def fetch_values(self, sample_name, module, data_type, note_type=None):
+        # Return values of search criteria that match
+        if sample_name not in self.get_sample_names():
+            raise _QCReportError("Sample '%s' not found in QCReport!" % sample_name)
+
+        elif module not in self.get_modules():
+            raise _QCReportError("Module '%s' not found in QCReport!" % module)
+
+        elif data_type not in self.get_colnames():
+            raise _QCReportError("Data type'%s' not found in QCReport!" % data_type)
+
+        # Add values of any datapoints which match filters
+        values = []
+        for sample_data_point in self.report[sample_name]:
+            if sample_data_point["Module"] == module and sample_data_point["Name"] == data_type:
+                if note_type is None:
+                    values.append(sample_data_point["Value"])
+                elif note_type == sample_data_point["Note"]:
+                    values.append(sample_data_point["Value"])
+        return values
+
+    def get_sample_names(self):
+        return self.report.keys()
+
+    def get_colnames(self, sample=None):
+        if sample is None:
+            sample_names = self.get_sample_names()
+            if len(sample_names) == 0:
+                return []
+            sample = sample_names[0]
+        # Get data colnames associated with a sample
+        return [x["Name"] for x in self.get_sample_data(sample)]
+
+    def get_modules(self, sample=None):
+        # Get list of modules used to produce QCReport data
+        if sample is None:
+            sample_names = self.get_sample_names()
+            if len(sample_names) == 0:
+                return []
+            sample = sample_names[0]
+        return [x["Module"] for x in self.get_sample_data(sample)]
+
+    def get_sample_data(self, sample):
+        if sample not in self.get_sample_names():
+            logging.error("Sample '%s' not found in QCReport!")
+            raise _QCReportError("Cannot get data of non-existant sample!")
+        return self.report[sample]
+
+    def validate(self):
+        # Determine whether QCReport is valid
+        for sample_name, sample_data in self.report.iteritems():
+            # Make sure every data point in every sample row contains only the required fields
+            for sample_column in sample_data:
+                if not "".join(sorted(sample_column.keys())) == "ModuleNameNoteSourceValue":
+                    logging.error("Entry in QCReport for sample %s does not contain required columns!" % sample_name)
+                    raise _QCReportError("Invalid QCReport schema.")
+
+        # Check to make sure QCReport is square
+        if not self.is_square():
+            # Check if
+            logging.error("QCReport is not square! Not all rows have same number of columns!")
+            raise _QCReportError("Invalid QCReport! Not all rows have same number of columns!")
+
+        # Check to make sure all rows in QCReport have columns in same order
+        if not self.is_ordered():
+            logging.error("QCReport is not ordered! Data columns are not same for every sample or are not in same order!")
+            raise _QCReportError("Invalid QCReport! Data columns are not same for every sample or are not in same order!")
+
+    def is_square(self):
+        # Return True if all rows have same number of columns
+        row_len = -1
+        for sample in self.get_sample_names():
+            if row_len == -1:
+                row_len = len(self.get_colnames(sample))
+            else:
+                if len(self.get_colnames(sample)) != row_len:
+                    return False
+        return True
+
+    def is_ordered(self):
+        # Return True if columns in every row are in same order
+        row_order = ""
+        for sample in self.get_sample_names():
+            if row_order == "":
+                row_order = "_".join(self.get_colnames(sample))
+            elif "_".join(self.get_colnames(sample)) != row_order:
+                return False
+        return True
+
+
+class _QCReportReader(Module):
+    # Private base class for methods shared by modules that parse data from QCReports and
+    # Make that data available to downstream modules in the pipelines
     def __init__(self, module_id, is_docker=False):
-        super(QCReportReader, self).__init__(module_id, is_docker)
+        super(_QCReportReader, self).__init__(module_id, is_docker)
 
     def define_input(self):
         self.add_argument("qc_report",      is_required=True)
@@ -11,20 +135,13 @@ class QCReportReader(Module):
         self.add_argument("nr_cpus",        is_required=True,   default_value=1)
         self.add_argument("mem",            is_required=True,   default_value=1)
 
-    def define_output(self):
-        pass
-
     def define_command(self):
         # Spit qc report to stdout for parsing
         qc_report       = self.get_argument("qc_report")
         return "cat %s !LOG2!" % qc_report
 
-    @staticmethod
-    def __parse_qc_report(out):
-        # Return QCReport parsed from stdout
-        pass
 
-class GetNumReads(QCReportReader):
+class GetNumReads(_QCReportReader):
 
     def __init__(self, module_id, is_docker=False):
         super(GetNumReads, self).__init__(module_id, is_docker)
@@ -32,19 +149,39 @@ class GetNumReads(QCReportReader):
 
     def define_input(self):
         super(GetNumReads, self).define_input()
-        self.add_argument("filter_by_note")
+        self.add_argument("filter_by_note", default_value=None)
 
     def define_output(self):
         self.add_output("nr_reads", 0, is_path=False)
 
     def process_cmd_output(self, out, err):
         # Parse numreads from FastQC sections of QCReport
-        qc_report = self.__parse_qc_report(out)
-
+        qc_report = parse_qc_report(out)
         sample_name     = self.get_argument("sample_name")
         filter_by_note  = self.get_argument("filter_by_note")
-        num_reads       = sum(qc_report.fetch_values(sample_name,
-                                                     module="FastQC",
-                                                     key="Total_Reads",
-                                                     note=filter_by_note))
+
+        # Try to parse num_reads from QCReport
+        try:
+            num_reads = qc_report.fetch_values(sample_name,
+                                               module="FastQC",
+                                               data_type="Total_Reads",
+                                               note_type=filter_by_note)
+        # Raise any errors
+        except BaseException, e:
+            logging.error("GetNumReads unable to get number of reads from QCReport!")
+            if e.message != "":
+                logging.error("Receive the following err msg:\n%s" % e.message)
+            raise
+
+        # Get length of errors
+        if len(num_reads) == 0:
+            logging.error("GetNumReads could not find any FastQC num reads which matched the search criteria!")
+            raise RuntimeError("GetNumReads could not determine the number of FastQC reads from the QCReport!")
+
+        # Sum R1, R2 for paired end reads (will also work for single end reads where only one record is present)
+        num_reads = sum(num_reads)
+
+        # Update num reads
         self.set_output("nr_reads", num_reads)
+        logging.debug("Dis how many reads we got: %s" % num_reads)
+
