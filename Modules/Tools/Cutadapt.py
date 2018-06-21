63b7d8d69eacf1c700054f19e13324d89096e67d
Tushar Dave <tushar.dave@duke.edu>
2018-06-21 17:12:08 -0400

    Incorporate Base module changes to cutadapt module

--- Modules/Tools/Cutadapt.py
+++ Modules/Tools/Cutadapt.py
@@ -3,12 +3,9 @@ import logging
 from Modules import Module
 
 class Cutadapt (Module):
-    def __init__(self, module_id):
-        super(Cutadapt, self).__init__(module_id)
-
-        self.input_keys     = ["R1", "fiveprime_adapter", "threeprime_adapter", "max_error_rate", "min_len", "max_len", "nr_cpus", "mem"]
+    def __init__(self, module_id, is_docker = False):
+        super(Cutadapt, self).__init__(module_id, is_docker)
         self.output_keys    = ["R1", "R1_unpair", "R1_untrimmed", "R1_toolong", "R1_tooshort"]
-        self.quick_command  = True
 
     def define_input(self):
         self.add_argument("R1",         is_required=True)
@@ -22,35 +19,34 @@ class Cutadapt (Module):
         self.add_argument("min_len",                is_required=True)
         self.add_argument("max_len",                is_required=True)
 
-    def define_output(self, platform, split_name=None):
+    def define_output(self):
 
         # Declare trimmed R1 output filename
         r1_trimmed_ext  = ".R1.trimmed.fastq"
-        r1_trimmed_out  = self.generate_unique_file_name(split_name=split_name, extension=r1_trimmed_ext)
-        self.add_output(platform, "R1", r1_trimmed_out)
+        r1_trimmed_out  = self.generate_unique_file_name(extension=r1_trimmed_ext)
+        self.add_output("R1", r1_trimmed_out)
 
         # Declare discarded untrimmed R1 filename
-        r1_untrimmed_out  = self.generate_unique_file_name(split_name=split_name, extension=".R1.untrimmed.fastq")
-        self.add_output(platform, "R1_untrimmed", r1_untrimmed_out)
+        r1_untrimmed_out  = self.generate_unique_file_name(extension=".R1.untrimmed.fastq")
+        self.add_output("R1_untrimmed", r1_untrimmed_out)
 
         # Declare discarded R1 too long filename
-        r1_toolong_out = self.generate_unique_file_name(split_name=split_name, extension=".R1.toolong.fastq")
-        self.add_output(platform, "R1_toolong", r1_toolong_out)
+        r1_toolong_out = self.generate_unique_file_name(extension=".R1.toolong.fastq")
+        self.add_output("R1_toolong", r1_toolong_out)
 
         # Declare discarded R1 too short filename
-        r1_tooshort_out = self.generate_unique_file_name(split_name=split_name, extension=".R1.tooshort.fastq")
-        self.add_output(platform, "R1_tooshort", r1_tooshort_out)
+        r1_tooshort_out = self.generate_unique_file_name(extension=".R1.tooshort.fastq")
+        self.add_output("R1_tooshort", r1_tooshort_out)
 
-    def define_command(self, platform):
-        # Generate command for running cutadapt
+    def define_command(self):
 
         # Get program options
-        R1                  = self.get_arguments("R1").get_value()
-        fiveprime_adapter   = self.get_arguments("fiveprime_adapter").get_value()
-        threeprime_adapter  = self.get_arguments("threeprime_adapter").get_value()
-        max_error_rate      = self.get_arguments("max_error_rate").get_value()
-        min_len             = self.get_arguments("min_len").get_value()
-        max_len             = self.get_arguments("max_len").get_value()
+        R1                  = self.get_argument("R1")
+        fiveprime_adapter   = self.get_argument("fiveprime_adapter")
+        threeprime_adapter  = self.get_argument("threeprime_adapter")
+        max_error_rate      = self.get_argument("max_error_rate")
+        min_len             = self.get_argument("min_len")
+        max_len             = self.get_argument("max_len")
 
         # Get output filenames
         R1_out              = self.get_output("R1")
@@ -58,12 +54,21 @@ class Cutadapt (Module):
         R1_toolong_out      = self.get_output("R1_toolong")
         R1_tooshort_out     = self.get_output("R1_tooshort")
 
-        # Generate command for single-end trimmomatic
-        cmd = "sudo pip install cutadapt ; "
-        cmd += "cutadapt -g ^%s -a %s -e %s %s -n 2 -m %s -M %s --too-short-output %s --too-long-output %s --untrimmed-output %s > %s" % (fiveprime_adapter, threeprime_adapter,
-                                                                                                                                          max_error_rate,
-                                                                                                                                          R1, min_len,
-                                                                                                                                          max_len, R1_tooshort_out,
-                                                                                                                                          R1_toolong_out, R1_untrimmed_out,
-                                                                                                                                          R1_out)
+        if not self.is_docker:
+
+            # Generate command for single-end trimmomatic
+            cmd = "sudo pip install cutadapt ; "
+            cmd += "cutadapt -g ^{0} -a {1} -e {2} {3} -n 2 -m {4} -M {5} --too-short-output {6} --too-long-output {7} " \
+                   "--untrimmed-output {8} > {9}".format(fiveprime_adapter, threeprime_adapter, max_error_rate, R1,
+                                                         min_len, max_len, R1_tooshort_out, R1_toolong_out, R1_untrimmed_out,
+                                                         R1_out)
+
+        else:
+
+            cmd = "cutadapt -g ^{0} -a {1} -e {2} {3} -n 2 -m {4} -M {5} --too-short-output {6} --too-long-output {7} " \
+                   "--untrimmed-output {8} > {9}".format(fiveprime_adapter, threeprime_adapter, max_error_rate, R1,
+                                                         min_len, max_len, R1_tooshort_out, R1_toolong_out,
+                                                         R1_untrimmed_out,
+                                                         R1_out)
+
         return cmd
