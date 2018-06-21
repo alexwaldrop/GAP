332254b0322105256d6ac31e3ac9ae8979424c9f
Tushar Dave <tushar.dave@duke.edu>
2018-06-21 17:12:40 -0400

    Incorporate Base module changes to delly module

--- Modules/Tools/Delly.py
+++ Modules/Tools/Delly.py
@@ -1,14 +1,9 @@
 from Modules import Module
 
 class Delly(Module):
-    def __init__(self, module_id):
-        super(Delly, self).__init__(module_id)
-
-        self.input_keys = ["bam", "is_tumor", "ref", "exclude_list", "delly", "nr_cpus", "mem"]
-
+    def __init__(self, module_id, is_docker = False):
+        super(Delly, self).__init__(module_id, is_docker)
         self.output_keys = ["bcf", "csi"]
-
-        self.quick_command = True
 
     def define_input(self):
         self.add_argument("bam",            is_required=True)
@@ -19,30 +14,33 @@ class Delly(Module):
         self.add_argument("nr_cpus",        is_required=True, default_value=2)
         self.add_argument("mem",            is_required=True, default_value=13)
 
-    def define_output(self, platform, split_name=None):
+    def define_output(self):
 
         # Declare unique file name for bcf file
-        bcf_file = self.generate_unique_file_name(split_name=split_name, extension=".bcf")
-        self.add_output(platform, "bcf", bcf_file)
+        bcf_file = self.generate_unique_file_name(extension=".bcf")
+        self.add_output("bcf", bcf_file)
 
         # Declare the path to the csi index file
         csi_file = bcf_file + ".csi"
-        self.add_output(platform, "csi", csi_file)
+        self.add_output("csi", csi_file)
 
-    def define_command(self, platform):
+    def define_command(self):
 
         # Get arguments to run Delly
-        bam             = self.get_arguments("bam").get_value()
-        is_tumor        = self.get_arguments("is_tumor").get_value()
-        ref             = self.get_arguments("ref").get_value()
-        exclude_list    = self.get_arguments("exclude_list").get_value()
-        delly           = self.get_arguments("delly").get_value()
+        bam             = self.get_argument("bam")
+        is_tumor        = self.get_argument("is_tumor")
+        ref             = self.get_argument("ref")
+        exclude_list    = self.get_argument("exclude_list")
+        delly           = self.get_argument("delly")
 
         # Get output paths
         bcf             = self.get_output("bcf")
 
-        # Get libraries path
-        libs_path = "{0}/modular-boost/stage/lib/:{0}/htslib/".format(delly.rsplit("/",1)[0])
+        libs_path = None
+
+        if not self.is_docker:
+            # Get libraries path
+            libs_path = "{0}/modular-boost/stage/lib/:{0}/htslib/".format(delly.rsplit("/",1)[0])
 
         # Identify the tumor and the normal
         if is_tumor[0]:
@@ -53,11 +51,17 @@ class Delly(Module):
             normal = bam[0]
 
         # Generate command
-        if exclude_list:
-            cmd = "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:%s ; %s call -x %s -g %s -o %s %s %s !LOG3!" \
-                  % (libs_path, delly, exclude_list, ref, bcf, tumor, normal)
+        if not self.is_docker:
+            if exclude_list:
+                cmd = "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:{0} ; {1} call -x {2} -g {3} -o {4} {5} {6} !LOG3!".format\
+                    (libs_path, delly, exclude_list, ref, bcf, tumor, normal)
+            else:
+                cmd = "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:{0} ; {1} call -g {2} -o {3} {4} {5} !LOG3!".format\
+                    (libs_path, delly, ref, bcf, tumor, normal)
         else:
-            cmd = "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:%s ; %s call -g %s -o %s %s %s !LOG3!" \
-                  % (libs_path, delly, ref, bcf, tumor, normal)
+            if exclude_list:
+                cmd = "{0} call -x {1} -g {2} -o {3} {4} {5} !LOG3!".format(delly, exclude_list, ref, bcf, tumor, normal)
+            else:
+                cmd = "{0} call -g {1} -o {2} {3} {4} !LOG3!".format(delly, ref, bcf, tumor, normal)
 
         return cmd
