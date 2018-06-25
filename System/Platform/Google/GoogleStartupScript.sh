8422b3bc8837db0dca6da01ff792545ee5d5f76e
Alex Waldrop <amw117@duke.edu>
2018-06-25 15:27:36 -0400

    Fixed bug in GoogleStartupScript.sh.
    Gcloud update required --zone as input for add-metadata.
    Added code to determine instance zone on fly and use it to change 'READY' metadata.

--- System/Platform/Google/GoogleStartupScript.sh
+++ System/Platform/Google/GoogleStartupScript.sh
@@ -25,4 +25,5 @@ sudo aptdcon --hide-terminal -c
 ############################################################################################*
 
 # Signal that instance is fully initialized
-gcloud --quiet compute instances add-metadata $(hostname) --metadata READY=TRUE
+ZONE=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/zone" -H "Metadata-Flavor: Google" | cut -d "/" -f 4)
+gcloud --quiet compute instances add-metadata $(hostname) --metadata READY=TRUE --zone ${ZONE}
