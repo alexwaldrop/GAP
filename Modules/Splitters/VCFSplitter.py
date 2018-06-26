e130ae356a234b522123247eab936d0e64d1d44e
Alex Waldrop <amw117@duke.edu>
2018-06-26 14:44:25 -0400

    Updated VCFSplitter module.

--- Modules/Splitters/VCFSplitter.py
+++ Modules/Splitters/VCFSplitter.py
@@ -1,43 +1,38 @@
-from Modules import Module
+from Modules import Splitter
 
-class VCFSplitter(Module):
+class VCFSplitter(Splitter):
 
-    def __init__(self, module_id):
-        super(VCFSplitter, self).__init__(module_id)
-
-        self.input_keys     = ["vcf", "snpsift", "java", "nr_cpus", "mem"]
+    def __init__(self, module_id, is_docker=False):
+        super(VCFSplitter, self).__init__(module_id, is_docker)
         self.output_keys    = ["vcf"]
-        self.quick_command  = True
 
     def define_input(self):
         self.add_argument("vcf",                is_required=True)
         self.add_argument("snpsift",            is_required=True, is_resource=True)
-        self.add_argument("java",               is_required=True, is_resource=True)
+        self.add_argument("vcf_chrom_list",     is_required=True)
         self.add_argument("nr_cpus",            is_required=True, default_value=1)
         self.add_argument("mem",                is_required=True, default_value=1)
 
-    def define_output(self, platform, split_name=None):
+        # Conditionally require java if docker not provided
+        if not self.is_docker:
+            self.add_argument("java", is_required=True, is_resource=True)
 
+    def define_output(self):
         # Get names of chromosomes in VCF file
-        vcf_in          = self.get_arguments("vcf").get_value()
-        basename = vcf_in.split(".vcf")[0]
-        get_names_cmd   = 'cat %s | grep -v "#" | cut -f1 | sort | uniq' % vcf_in
-        out, err        = platform.run_quick_command("get_vcf_chroms", get_names_cmd)
+        vcf_in = self.get_argument("vcf")
+        for chrom in self.get_argument("vcf_chrom_list"):
+            vcf_out = "{0}.{1}.vcf".format(vcf_in, chrom)
+            self.make_split(split_id=chrom)
+            self.add_output(split_id=chrom, key="vcf", value=vcf_out)
 
-        for line in out.split("\n"):
-            # Skip empty lines
-            if len(line) > 0:
-                # Create split for next chromosome
-                chrom   = line
-                vcf_out = "%s.%s.vcf" % (basename, chrom)
-                self.add_output(platform, chrom, {"vcf":vcf_out}, is_path=False)
-
-    def define_command(self, platform):
+    def define_command(self):
         # Get input arguments
-        vcf_in      = self.get_arguments("vcf").get_value()
-        snpsift     = self.get_arguments("snpsift").get_value()
-        java        = self.get_arguments("java").get_value()
+        vcf_in      = self.get_argument("vcf")
+        snpsift     = self.get_argument("snpsift")
 
         # Generating SnpEff command
-        cmd = "%s -jar %s split %s !LOG3!" % (java, snpsift, vcf_in)
-        return cmd
+        if not self.is_docker:
+            java = self.get_argument("java")
+            return "%s -jar %s split %s !LOG3!" % (java, snpsift, vcf_in)
+        else:
+            return "%s split %s !LOG3!" % (snpsift, vcf_in)
