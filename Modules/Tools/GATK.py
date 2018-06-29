20597e892530a4027452a9d79ca09f7cf0a505f2
Alex Waldrop <amw117@duke.edu>
2018-06-29 18:13:17 -0400

    Made changes to GATK module.
    Moved Mutect2 and GenotypeGVCF subs to Mergers.

--- Modules/Tools/GATK.py
+++ Modules/Tools/GATK.py
@@ -2,103 +2,6 @@ import logging
 
 from Modules import Module
 
-class Mutect2(Module):
-
-    def __init__(self, module_id, is_docker=False):
-        super(Mutect2, self).__init__(module_id, is_docker)
-        self.output_keys = ["vcf", "vcf_idx"]
-
-    def define_input(self):
-        self.add_argument("bam",                is_required=True)
-        self.add_argument("bam_idx",            is_required=True)
-        self.add_argument("sample_name",        is_required=True)
-        self.add_argument("is_tumor",           is_required=True)
-        self.add_argument("gatk",               is_required=True,   is_resource=True)
-        self.add_argument("ref",                is_required=True,   is_resource=True)
-        self.add_argument("germline_vcf",       is_required=False,  is_resource=True)
-        self.add_argument("nr_cpus",            is_required=True,   default_value=8)
-        self.add_argument("mem",                is_required=True,   default_value=30)
-        self.add_argument("location")
-        self.add_argument("excluded_location")
-
-        # Require java if not being run in docker environment
-        if not self.is_docker:
-            self.add_argument("java", is_required=True, is_resource=True)
-
-    def define_output(self):
-        # Declare VCF output filename
-        vcf = self.generate_unique_file_name(extension=".vcf")
-        self.add_output("vcf", vcf)
-        # Declare VCF index output filename
-        vcf_idx = self.generate_unique_file_name(extension=".vcf.idx")
-        self.add_output("vcf_idx", vcf_idx)
-
-    def define_command(self):
-        # Get input arguments
-        bams            = self.get_argument("bam")
-        sample_names    = self.get_argument("sample_name")
-        is_tumor        = self.get_argument("is_tumor")
-        gatk            = self.get_argument("gatk")
-        ref             = self.get_argument("ref")
-        germline_vcf    = self.get_argument("germline_vcf")
-        L               = self.get_argument("location")
-        XL              = self.get_argument("excluded_location")
-        nr_cpus         = self.get_argument("nr_cpus")
-        mem             = self.get_argument("mem")
-        vcf             = self.get_output("vcf")
-
-        # Generate command with java if not running on docker
-        if not self.is_docker:
-            java = self.get_argument("java")
-            jvm_options = "-Xmx%dG -Djava.io.tmpdir=%s" % (mem * 4 / 5, "/tmp/")
-            cmd = "%s %s -jar %s Mutect2" % (java, jvm_options, gatk)
-
-        # Generate base command with endpoint provided by docker
-        else:
-            cmd = "%s Mutect2" % gatk
-
-        # Generating the MuTect2 options
-        opts = list()
-
-        # Add Tumor/Normal sample names
-        if is_tumor[0]:
-            opts.append("-tumor %s" % sample_names[0])
-            opts.append("-normal %s" % sample_names[1])
-        else:
-            opts.append("-tumor %s" % sample_names[1])
-            opts.append("-normal %s" % sample_names[0])
-
-        # Add arguments for bams
-        tumor_bams = ["-I %s" % bam for bam in bams[0] ] if isinstance(bams[0], list) else ["-I %s" % bams[0]]
-        normal_bams = ["-I %s" % bam for bam in bams[1] ] if isinstance(bams[1], list) else ["-I %s" % bams[1]]
-        opts += tumor_bams + normal_bams
-
-        opts.append("-O %s" % vcf)
-        opts.append("-R %s" % ref)
-        opts.append("--native-pair-hmm-threads %s" % nr_cpus)
-
-        if germline_vcf is not None:
-            opts.append("--germline-resource %s" % germline_vcf)
-
-        # Limit the locations to be processes
-        if L is not None:
-            if isinstance(L, list):
-                for included in L:
-                    if included != "unmapped":
-                        opts.append("-L \"%s\"" % included)
-            else:
-                opts.append("-L \"%s\"" % L)
-        if XL is not None:
-            if isinstance(XL, list):
-                for excluded in XL:
-                    opts.append("-XL \"%s\"" % excluded)
-            else:
-                opts.append("-XL \"%s\"" % XL)
-
-        # Generating command for Mutect2
-        return "%s %s !LOG3!" % (cmd, " ".join(opts))
-
-
 class HaplotypeCaller(Module):
 
     def __init__(self, module_id, is_docker=False):
@@ -300,7 +203,7 @@ class BaseRecalibrator(Module):
 
         # Generate base command with endpoint provided by docker
         else:
-            cmd = "%s -T BaseRecalibrator" % gatk
+            cmd = "%s BaseRecalibrator" % gatk
 
         # Generating the base recalibration options
         opts = list()
@@ -347,85 +250,6 @@ class BaseRecalibrator(Module):
                 return chrom_list
         # If here, then process the entire file
         return None
-
-
-class GenotypeGVCFs(Module):
-
-    def __init__(self, module_id, is_docker=False):
-        super(GenotypeGVCFs, self).__init__(module_id, is_docker)
-        self.output_keys = ["vcf", "vcf_idx"]
-
-    def define_input(self):
-        self.add_argument("gvcf",                is_required=True)
-        self.add_argument("gvcf_idx",            is_required=True)
-        self.add_argument("gatk",               is_required=True, is_resource=True)
-        self.add_argument("ref",                is_required=True, is_resource=True)
-        self.add_argument("nr_cpus",            is_required=True, default_value=6)
-        self.add_argument("mem",                is_required=True, default_value=35)
-        self.add_argument("location")
-        self.add_argument("excluded_location")
-
-        # Require java if not being run in docker environment
-        if not self.is_docker:
-            self.add_argument("java", is_required=True, is_resource=True)
-
-    def define_output(self):
-        # Declare VCF output filename
-        vcf = self.generate_unique_file_name(extension=".vcf")
-        self.add_output("vcf", vcf)
-        # Declare VCF index output filename
-        vcf_idx = self.generate_unique_file_name(extension=".vcf.idx")
-        self.add_output("vcf_idx", vcf_idx)
-
-    def define_command(self):
-        # Get input arguments
-        gvcf_in = self.get_argument("gvcf")
-        gatk    = self.get_argument("gatk")
-        ref     = self.get_argument("ref")
-        L       = self.get_argument("location")
-        XL      = self.get_argument("excluded_location")
-        mem     = self.get_argument("mem")
-        vcf     = self.get_output("vcf")
-
-        # Generate command with java if not running on docker
-        if not self.is_docker:
-            java = self.get_argument("java")
-            jvm_options = "-Xmx%dG -Djava.io.tmpdir=%s" % (mem * 4 / 5, "/tmp/")
-            cmd = "%s %s -jar %s -T GenotypeGVCFs" % (java, jvm_options, gatk)
-
-        # Generate base command with endpoint provided by docker
-        else:
-            cmd = "%s -T GenotypeGVCFs" % gatk
-
-
-        # Generating the haplotype caller options
-        opts = list()
-
-        if isinstance(gvcf_in, list):
-            for gvcf in gvcf_in:
-                opts.append("--variant %s" % gvcf)
-        else:
-            opts.append("--variant %s" % gvcf_in)
-        opts.append("-o %s" % vcf)
-        opts.append("-R %s" % ref)
-
-        # Limit the locations to be processes
-        if L is not None:
-            if isinstance(L, list):
-                for included in L:
-                    if included != "unmapped":
-                        opts.append("-L \"%s\"" % included)
-            else:
-                opts.append("-L \"%s\"" % L)
-        if XL is not None:
-            if isinstance(XL, list):
-                for excluded in XL:
-                    opts.append("-XL \"%s\"" % excluded)
-            else:
-                opts.append("-XL \"%s\"" % XL)
-
-        # Generating command for GenotypeGVCFs
-        return "touch *.idx; %s %s !LOG3!" % (cmd,  " ".join(opts))
 
 
 class IndexVCF(Module):
