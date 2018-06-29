3e68b7388cb9edea335c04a374cf3f2500e828d9
Alex Waldrop <amw117@duke.edu>
2018-06-29 18:14:50 -0400

    Added SortVCF sub to Picard module.

--- Modules/Tools/Picard.py
+++ Modules/Tools/Picard.py
@@ -179,3 +179,59 @@ class SamToFastq(Module):
 
         # Convert to FASTQ the final values
         return "%s SamToFastq I=%s F=%s F2=%s VALIDATION_STRINGENCY=LENIENT !LOG3!" % (basecmd, bam, R1, R2)
+
+
+class SortGVCF(Module):
+
+    def __init__(self, module_id, is_docker=False):
+        super(SortGVCF, self).__init__(module_id, is_docker)
+        self.output_keys = ["gvcf", "gvcf_idx"]
+
+    def define_input(self):
+        self.add_argument("gvcf",               is_required=True)
+        self.add_argument("gvcf_idx",           is_required=True)
+        self.add_argument("picard",             is_required=True, is_resource=True)
+        self.add_argument("ref",                is_required=True, is_resource=True)
+        self.add_argument("nr_cpus",            is_required=True, default_value=4)
+        self.add_argument("mem",                is_required=True, default_value=25)
+
+        # Require java if not being run on docker
+        if not self.is_docker:
+            self.add_argument("java", is_required=True, is_resource=True)
+
+    def define_output(self):
+
+        # Generate a path for the GVCF file
+        gvcf_out = self.generate_unique_file_name(extension=".sorted.g.vcf")
+        self.add_output("gvcf", gvcf_out)
+
+        # Generate a path for the GVCF index file
+        gvcf_idx = gvcf_out + ".idx"
+        self.add_output("gvcf_idx", gvcf_idx)
+
+    def define_command(self):
+        # Get input arguments
+        gvcf    = self.get_argument("gvcf")
+        picard  = self.get_argument("picard")
+        ref     = self.get_argument("ref")
+
+        # Get output file
+        gvcf_out    = self.get_output("gvcf")
+
+        # Set JVM options
+        if not self.is_docker:
+            java    = self.get_argument("java")
+            mem     = self.get_argument("mem")
+            jvm_options = "-Xmx%dG -Djava.io.tmpdir=/tmp/" % (mem * 9 / 10)
+            picard_cmd = "{0} {1} -jar {2}".format(java, jvm_options, picard)
+        else:
+            picard_cmd = str(picard)
+
+        # Generating the options
+        opts = list()
+        opts.append("I=%s" % gvcf)
+        opts.append("O=%s" % gvcf_out)
+        opts.append("SD=%s" % ref.replace(".fasta", ".dict").replace(".fa", ".dict"))
+
+        # Generating command for base recalibration
+        return "{0} SortVcf {1} !LOG3!".format(picard_cmd, " ".join(opts))
