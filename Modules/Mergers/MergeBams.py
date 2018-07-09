a17ad63b6151e5e8b2ed3ffb480645c1bf16ba61
Alex Waldrop <amw117@duke.edu>
2018-07-09 16:41:50 -0400

    Fixed MergeBams to handle single input case.

--- Modules/Mergers/MergeBams.py
+++ Modules/Mergers/MergeBams.py
@@ -1,3 +1,5 @@
+import copy
+
 from Modules import Merger
 
 class MergeBams(Merger):
@@ -8,26 +10,43 @@ class MergeBams(Merger):
 
     def define_input(self):
         self.add_argument("bam",            is_required=True)
+        self.add_argument("bam_idx",        is_required=True)
         self.add_argument("bam_sorted",     is_required=True, default_value=True)
         self.add_argument("samtools",       is_required=True, is_resource=True)
         self.add_argument("nr_cpus",        is_required=True, default_value=8)
         self.add_argument("mem",            is_required=True, default_value="nr_cpus * 2")
 
     def define_output(self):
-        # Declare merged bam output file
-        bam_out = self.generate_unique_file_name(extension=".bam")
-        bam_idx = "%s.bai" % bam_out
+        # Declare merged bam output
+
+        # Single bam input. No merging to do.
+        if not isinstance(self.get_argument("bam"), list):
+            bam_out = copy.deepcopy(self.arguments["bam"].get_value())
+            bam_idx = copy.deepcopy(self.arguments["bam_idx"].get_value())
+
+        # Multiple bam input. Merge into single bam.
+        else:
+            bam_out = self.generate_unique_file_name(extension=".bam")
+            bam_idx = "%s.bai" % bam_out
+
         self.add_output("bam",      bam_out)
         self.add_output("bam_idx",  bam_idx)
 
     def define_command(self):
         # Obtaining the arguments
         bam_list        = self.get_argument("bam")
+        bam_idx_list    = self.get_argument("bam_idx")
         samtools        = self.get_argument("samtools")
         nr_cpus         = self.get_argument("nr_cpus")
         sorted_input    = self.get_argument("bam_sorted")
         output_bam      = self.get_output("bam")
         output_bam_idx  = self.get_output("bam_idx")
+
+        needs_merging   = bam_list != output_bam.get_path()
+
+        # Don't do anything if single bam that already has index
+        if not needs_merging:
+            return None
 
         # Generating the merging command
         if sorted_input:
